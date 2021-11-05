24bb3d3fef4e1c8a442d85d081111204a2d1b503
Marcus G K Williams <marcus.williams@intel.com>
2021-11-05 07:45:41 -0700

    Fix dist bug
    Signed-off-by: Marcus G K Williams <marcus.williams@intel.com>

--- build.py
+++ build.py
@@ -21,7 +21,7 @@ license = ["BSD-3-Clause", "LGPL-2.1"]
 
 @init
 def set_properties(project):
-    project.set_property("dir_source_main_python", "lava")
+    project.set_property("dir_source_main_python", "src/python")
     project.set_property("dir_source_unittest_python", "tests")
     project.set_property("dir_source_main_scripts", "scripts")
     project.set_property("dir_docs", "docs")
