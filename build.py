d64d759d9c5063d1c8118ee622a766500087bfae
Marcus G K Williams <marcus.williams@intel.com>
2021-11-05 09:53:40 -0700

    CI, update unit test runs
    Signed-off-by: Marcus G K Williams <marcus.williams@intel.com>

--- build.py
+++ build.py
@@ -22,7 +22,7 @@ license = ["BSD-3-Clause", "LGPL-2.1"]
 @init
 def set_properties(project):
     project.set_property("dir_source_main_python", "src/python")
-    project.set_property("dir_source_unittest_python", "tests")
+    project.set_property("dir_source_unittest_python", "tests/python")
     project.set_property("dir_source_main_scripts", "scripts")
     project.set_property("dir_docs", "docs")
 
@@ -63,7 +63,7 @@ def set_properties(project):
 @init(environments="unit")
 def set_properties_unit(project):
     project.set_property("dir_source_main_python", "src/python")
-    project.set_property("dir_source_unittest_python", "tests")
+    project.set_property("dir_source_unittest_python", "tests/python")
     project.set_property("dir_source_main_scripts", "scripts")
     project.set_property("dir_docs", "docs")
 
