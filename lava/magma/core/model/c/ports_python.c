d3cd0d4957991999b0f08a64005d5ba917dc688e
jlakness-intel <84150418+jlakness-intel@users.noreply.github.com>
2021-11-08 17:11:21 -0800

    filled out python ports implementation

--- lava/magma/core/model/c/ports_python.c
+++ lava/magma/core/model/c/ports_python.c
@@ -1,32 +1,45 @@
 #define PYTHON
-#include <python.h>
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#include <Python.h>
+#include "numpy/arrayobject.h"
 #include <assert.h>
 #include <stdint.h>
 #include "ports.h"
 
 size_t port_send(PyObject *port, void *data){ // send via pointer
     PyObject *pyDataObj; // replace with some wrapping of data
-    PyObject* n = PyObject_CallMethod(port,"send","O",pyDataObj); // call python port method directly
-    assert(PyLong_Check(n));
-    return PyLong_AsSize_t((PyLongObject*)n); 
+    PyObject* result = PyObject_CallMethod(port,"send","O",pyDataObj); // call python port method directly
+    assert(PyLong_Check(result));
+    size_t n  = PyLong_AsSize_t((PyLongObject*)n); 
+    Py_DECREF(result);
+    return n;
 }
 
 size_t port_recv(PyObject *port,void** data){ // recieve pointer to pointer
     PyObject *pyDataObj = PyObject_CallMethod(port,"recv",NULL); // call python port object method
-    PyObject *n = PyObject_CallMethod(pyDataObj,"__len__",NULL); // call len(obj)
-    *data = (void*) 0; // TODO: replace with assignment of C-format data from pyDataObj
-    assert(PyLong_Check(n));
-    return PyLong_AsSize_t((PyLongObject*)n); 
+    PyObject *result = PyObject_CallMethod(pyDataObj,"__len__",NULL); // call len(obj)
+    assert(PyLong_Check(result));
+    size_t n  = PyLong_AsSize_t((PyLongObject*)n); 
+    Py_DECREF(result);
+    PyObject *arrayObj = PyArray_ContiguousFromObject(pyDataObj,NPY_INT,0,0);
+    PyDECREF(pyDataObj);
+    *data = PyArray_DATA(arrayObj);
+    PyDECREF(pyDataObj); // WARNING: Not sure what to do about this. Possible deallocated pointer or leave in for possible memory leak 
+    return n; 
 }
 
 size_t port_peek(PyObject *port){ // simple call and return of simple type
-    PyObject *n = PyObject_CallMethod(port,"peek",NULL);
-    assert(PyLong_Check(n));
-    return PyLong_AsSize_t((PyLongObject*)n); 
+    PyObject *result = PyObject_CallMethod(port,"peek",NULL);
+    assert(PyLong_Check(result));
+    size_t n  = PyLong_AsSize_t((PyLongObject*)n); 
+    Py_DECREF(result);
+    return n;
 }
 
 size_t port_probe(PyObject *port){ // simple call and return of simple type
-    PyObject *n = PyObject_CallMethod(port,"probe",NULL);
-    assert(PyLong_Check(n));
-    return PyLong_AsSize_t((PyLongObject*)n); 
+    PyObject *result = PyObject_CallMethod(port,"probe",NULL);
+    assert(PyLong_Check(result));
+    size_t n  = PyLong_AsSize_t((PyLongObject*)n); 
+    Py_DECREF(result);
+    return n;
 }
