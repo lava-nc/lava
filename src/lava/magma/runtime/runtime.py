342457c2e515738255774e66a79f09cf440e586a
PhilippPlank <philipp.plank@intel.com>
2021-11-17 19:26:28 +0100

    - Changed management csp channels to use float instead of int - enables get/set of a Var to use floats - modified unit tests to also check for data type

--- src/lava/magma/runtime/runtime.py
+++ src/lava/magma/runtime/runtime.py
@@ -10,7 +10,7 @@ import numpy as np
 from lava.magma.compiler.channels.pypychannel import CspSendPort, CspRecvPort
 from lava.magma.compiler.exec_var import AbstractExecVar
 from lava.magma.core.process.message_interface_enum import ActorType
-from lava.magma.runtime.message_infrastructure.message_infrastructure_interface\
+from lava.magma.runtime.message_infrastructure.message_infrastructure_interface \
     import MessageInfrastructureInterface
 from lava.magma.runtime.message_infrastructure.factory import \
     MessageInfrastructureFactory
@@ -322,7 +322,7 @@ class Runtime:
                 runtime_srv_id]
             data_port.send(enum_to_np(num_items))
             for i in range(num_items):
-                data_port.send(enum_to_np(buffer[0, i]))
+                data_port.send(np.array([buffer[0, i]], dtype=np.float64))
         else:
             raise RuntimeError("Runtime has not started")
 
@@ -352,7 +352,7 @@ class Runtime:
             # 2. Receive Data [NUM_ITEMS, DATA1, DATA2, ...]
             data_port: CspRecvPort = self.service_to_runtime_data[
                 runtime_srv_id]
-            num_items: int = data_port.recv()[0].item()
+            num_items: int = int(data_port.recv()[0].item())
             buffer: np.ndarray = np.empty((1, num_items))
             for i in range(num_items):
                 buffer[0, i] = data_port.recv()[0]
