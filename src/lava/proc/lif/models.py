6e49476a4a8eeabe57e88843a0b249beaae227ca
Risbud, Sumedh <sumedh.risbud@intel.com>
2021-11-18 10:22:21 -0800

    LIF: changes in response to @mathisrichter's review
    Signed-off-by: Risbud, Sumedh <sumedh.risbud@intel.com>

--- src/lava/proc/lif/models.py
+++ src/lava/proc/lif/models.py
@@ -16,6 +16,11 @@ from lava.proc.lif.process import LIF
 @requires(CPU)
 @tag('floating_pt')
 class PyLifModelFloat(PyLoihiProcessModel):
+    """Implementation of Leaky-Integrate-and-Fire neural process in floating
+    point precision. This short and simple ProcessModel can be used for quick
+    algorithmic prototyping, without engaging with the nuances of a fixed
+    point implementation.
+    """
     a_in: PyInPort = LavaPyType(PyInPort.VEC_DENSE, np.float)
     s_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, bool, precision=1)
     u: np.ndarray = LavaPyType(np.ndarray, np.float)
@@ -81,6 +86,16 @@ class PyLifModelBitAcc(PyLoihiProcessModel):
         self.b_vth_computed = False
         self.effective_bias = 0
         self.effective_vth = 0
+        # Let's define some bit-widths from Loihi
+        # State variables u and v are 24-bits wide
+        self.uv_bitwidth = 24
+        self.max_uv_val = 2 ** (self.uv_bitwidth - 1)
+        # Decays need an MSB alignment with 12-bits
+        self.decay_shift = 12
+        self.decay_unity = 2 ** self.decay_shift
+        # Threshold and incoming activation are MSB-aligned using 6-bits
+        self.vth_shift = 6
+        self.act_shift = 6
 
     def run_spk(self):
         # Receive synaptic input
@@ -91,7 +106,7 @@ class PyLifModelBitAcc(PyLoihiProcessModel):
             self.effective_bias = np.left_shift(self.bias, self.bias_exp)
             # In Loihi, user specified threshold is just the mantissa, with a
             # constant exponent of 6
-            self.effective_vth = np.left_shift(self.vth, 6)
+            self.effective_vth = np.left_shift(self.vth, self.vth_shift)
             self.b_vth_computed = True
 
         # Update current
@@ -100,31 +115,32 @@ class PyLifModelBitAcc(PyLoihiProcessModel):
         # Below, u is promoted to int64 to avoid overflow of the product
         # between u and decay constant beyond int32. Subsequent right shift by
         # 12 brings us back within 24-bits (and hence, within 32-bits)
-        decayed_curr = np.int64(self.u) * (4096 - decay_const_u)
+        decayed_curr = np.int64(self.u) * (self.decay_unity - decay_const_u)
         decayed_curr = np.sign(decayed_curr) * np.right_shift(np.abs(
-            decayed_curr), 12)
+            decayed_curr), self.decay_shift)
         decayed_curr = np.int32(decayed_curr)
         # Hardware left-shifts synpatic input for MSB alignment
-        a_in_data = np.left_shift(a_in_data, 6)
+        a_in_data = np.left_shift(a_in_data, self.act_shift)
         # Add synptic input to decayed current
         decayed_curr += a_in_data
         # Check if value of current is within bounds of 24-bit. Overflows are
         # handled by wrapping around modulo 2 ** 23. E.g., (2 ** 23) + k
         # becomes k and -(2**23 + k) becomes -k
-        wrapped_curr = np.mod(decayed_curr, np.sign(decayed_curr) * (2 ** 23))
+        wrapped_curr = np.mod(decayed_curr,
+                              np.sign(decayed_curr) * self.max_uv_val)
         self.u[:] = wrapped_curr
         # Update voltage
         # --------------
         decay_const_v = self.dv + self.dm_offset
         # ToDo: make the exponent 23 configurable (see comment above current
         #  limits)
-        neg_voltage_limit = -np.int32(2 ** 23) + 1
-        pos_voltage_limit = np.int32(2 ** 23) - 1
+        neg_voltage_limit = -np.int32(self.max_uv_val) + 1
+        pos_voltage_limit = np.int32(self.max_uv_val) - 1
         # Decaying voltage similar to current. See the comment above to
         # understand the need for each of the operations below.
-        decayed_volt = np.int64(self.v) * (4096 - decay_const_v)
+        decayed_volt = np.int64(self.v) * (self.decay_unity - decay_const_v)
         decayed_volt = np.sign(decayed_volt) * np.right_shift(np.abs(
-            decayed_volt), 12)
+            decayed_volt), self.decay_shift)
         decayed_volt = np.int32(decayed_volt)
         updated_volt = decayed_volt + self.u + self.effective_bias
         self.v[:] = np.clip(updated_volt, neg_voltage_limit, pos_voltage_limit)
