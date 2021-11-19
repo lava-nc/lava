7d46883b3468157b34173b583820868a1a703beb
jlakness-intel <84150418+jlakness-intel@users.noreply.github.com>
2021-11-19 01:59:09 -0800

    more code generation

--- lava/magma/core/model/c/generate.py
+++ lava/magma/core/model/c/generate.py
@@ -2,14 +2,39 @@ import typing as ty
 import textwrap as tw
 
 
-def gen_function(name: str = "MyFunc"):
+def gen_proto_h(names: ty.List[str]):
+    return (
+        "#ifndef _RUN_H\n"
+        + "#define _RUN_H\n"
+        + "\n".join(f"void {name}();" for name in names)
+        + "\n#endif\n"
+    )
+
+
+def gen_proto_c(names: ty.List[str]) -> str:
     return tw.dedent(
-        f"""
+        "".join(
+            f"""
+            void {name}(){{
+            }}
+            """
+            for name in names
+        )
+    )
+
+
+def gen_methods(names: ty.List[str]):
+    return tw.dedent(
+        "".join(
+            f"""
         static PyObject* Custom_{name}(CustomObject *self,PyObject* Py_UNUSED(ignored)){{
             printf("{name} method called\\n");
+            {name}();
             Py_RETURN_NONE;
         }}
         """
+            for name in names
+        )
     )
 
 
@@ -19,9 +44,17 @@ def gen_entry(name: str = "MyFunc"):
     )
 
 
-def gen_methods(names: ty.List[str]) -> str:
+def gen_method_decls(names: ty.List[str]) -> str:
+    return "".join(
+        f"static PyObject* Custom_{name}(CustomObject *self,PyObject* Py_UNUSED(ignored));"
+        + "\n"
+        for name in names
+    )
+
+
+def gen_method_def(names: ty.List[str]) -> str:
     entries = ",\n            ".join(gen_entry(name) for name in names)
-    methods = tw.dedent(
+    return tw.dedent(
         f"""
         static PyMethodDef Custom_methods[] = {{
             {entries},
@@ -29,8 +62,6 @@ def gen_methods(names: ty.List[str]) -> str:
             }};
         """
     )
-    functions = "".join(gen_function(name) for name in names)
-    return tw.dedent(methods + functions)
 
 
 def gen_port_struct(names: ty.List[str]) -> str:
@@ -41,6 +72,7 @@ def gen_port_struct(names: ty.List[str]) -> str:
             {fields}
         }} ports;
         """
+        + "\n"
     )
 
 
@@ -51,8 +83,54 @@ def gen_ports_load(names: ty.List[str]):
     )
 
 
+def gen_init(ports: ty.List[str]) -> str:
+    return (
+        "static int Custom_init(CustomObject* self,PyObject* args,PyObject* Py_UNUSED(ignored)){\n"
+        + tw.indent(gen_ports_load(ports), "    ")
+        + "\n    return 0;\n}\n"
+    )
+
+
+def gen_methods_h(methods: ty.List[str], ports: ty.List[str]) -> str:
+    return (
+        "#ifndef _METHODS_H\n" + "#define _METHODS_H\n"
+        "static int Custom_init(CustomObject* self,PyObject* args,PyObject* Py_UNUSED(ignored));\n"
+        + gen_method_decls(methods)
+        + gen_method_def(methods)
+        + gen_port_struct(ports)
+        + "#endif\n"
+    )
+    pass
+
+
+def gen_methods_c(
+    methods: ty.List[str], ports: ty.List[str], header: str
+) -> str:
+    return (
+        tw.dedent(
+            """
+            #define PY_SSIZE_T_CLEAN
+            #include <Python.h>
+            #include "structmember.h"
+            #include <stdio.h>
+            #include "proto.h"\n
+            """
+        )
+        + gen_init(ports)
+        + "\n"
+        + gen_methods(methods)
+        + "\n"
+    )
+
+
 if __name__ == "__main__":
-    names = ["a", "b", "c"]
-    # print(gen_methods(names))
-    print(gen_port_struct(names))
-    print(gen_ports_load(names))
+    methods = ["a", "b", "c"]
+    ports = ["d", "e", "f"]
+    print("=====methods.h======")
+    print(gen_methods_h(methods, ports))
+    print("=====methods.c======")
+    print(gen_methods_c(methods, ports, "mystuff.h"))
+    print("=====proto.h========")
+    print(gen_proto_h(methods))
+    print("=====proto.c========")
+    print(gen_proto_c(methods))
