08b7a0828a04d3941dfd051627a63e23031f0b31
jlakness-intel <84150418+jlakness-intel@users.noreply.github.com>
2021-11-19 01:57:18 -0800

    runnable object with phase loop defined in C

---
+++ lava/magma/core/model/c/run.c
@@ -0,0 +1,40 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include "structmember.h"
+
+#include <stdio.h>
+
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#include "numpy/ndarraytypes.h"
+#include "numpy/ufuncobject.h"
+#include "numpy/npy_3kcompat.h"
+
+#define PYTHON
+#include "methods.h"
+
+static PyObject* Custom_run(PyObject *self,PyObject* Py_UNUSED(ignored)){
+    printf("test_runstate.c run function called\n");
+    PyObject *state_port = PyObject_GetAttrString(self, "service_to_process_cmd");
+    while(1){
+        PyObject *probe_obj = PyObject_CallMethod(state_port,"probe",NULL);
+        assert(PyLong_Check(probe_obj));
+        size_t probe_size  = PyLong_AsSize_t((PyLongObject*)probe_obj); 
+        Py_DECREF(probe_obj);
+        if(probe_size){
+            PyObject *recv_obj = PyObject_CallMethod(state_port,"recv",NULL);
+            assert(PyLong_Check(recv_obj));
+            size_t recv_phase  = PyLong_AsSize_t((PyLongObject*)recv_obj); 
+            Py_DECREF(recv_obj);
+            runState state = {
+                .phase = recv_phase,
+            };
+            if(state.phase){ 
+                run(&state);
+            } else { // stop if phase is zero
+                break;
+            }
+        }
+    }
+    Py_DECREF(state_port);
+    Py_RETURN_NONE;
+}
--- lava/magma/core/model/c/run.h
+++ lava/magma/core/model/c/run.h
@@ -4,6 +4,15 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/*
+// Generate this block in methods.h
+static PyObject* Custom_run(PyObject *self,PyObject* Py_UNUSED(ignored));
+static PyMethodDef Custom_methods[] = {
+    {"run",(PyCFunction)Custom_run,METH_NOARGS,"basic lava run loop"},
+    {NULL}
+};
+*/
+
 typedef struct runState {
     uint8_t phase;
 } runState;
