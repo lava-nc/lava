89b21f7ffe794ced3a8f16d38e4990864ddc153e
bamsumit <bam_sumit@hotmail.com>
2021-11-24 13:10:55 -0800

    Conv Process Implementation (#73)
    * Conv process implementation with unittests.
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * updated requirements and test inits
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * tweak unittests
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * fix merge conflict
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Fix to comments, fixed lif warnings, refactored source sink processes and their unittests
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * flesh source/sink nomenclature. Fix overflow/underflow bug. Resolved Sudmedh's comments
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * New ground truth based conv implementation test
    
    * type hinting fix
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * removed unnecessary commented code
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * typehinting consistency fix: np.array -> np.ndarray
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * static seeds in unittest
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * fixes for clean init merge
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    Co-authored-by: Risbud, Sumedh <srrisbud@gmail.com>

---
+++ tests/lava/proc/io/test_source_sink.py
@@ -0,0 +1,59 @@
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+# See: https://spdx.org/licenses/
+from typing import List
+import unittest
+import numpy as np
+
+from lava.magma.core.run_configs import RunConfig
+from lava.magma.core.run_conditions import RunSteps
+from lava.proc.io.source import RingBuffer as SendProcess
+from lava.proc.io.sink import RingBuffer as ReceiveProcess
+from lava.magma.core.model.py.model import PyLoihiProcessModel
+
+np.random.seed(7739)
+
+
+class TestRunConfig(RunConfig):
+    """Run configuration selects appropriate ProcessModel based on tag
+    """
+    def __init__(self, select_tag: str = 'fixed_pt'):
+        super(TestRunConfig, self).__init__(custom_sync_domains=None)
+        self.select_tag = select_tag
+
+    def select(
+        self, _, proc_models: List[PyLoihiProcessModel]
+    ) -> PyLoihiProcessModel:
+        for pm in proc_models:
+            if self.select_tag in pm.tags:
+                return pm
+        raise AssertionError('No legal ProcessModel found.')
+
+
+class TestSendReceive(unittest.TestCase):
+    """Tests for all SendProces and ReceiveProcess."""
+
+    def test_source_sink(self) -> None:
+        """Test whatever is being sent form source is received at sink."""
+        num_steps = 10
+        shape = (64, 64, 16)
+        input = np.random.randint(256, size=shape + (num_steps,))
+        input -= 128
+        # input = 0.5 * input
+
+        source = SendProcess(data=input)
+        sink = ReceiveProcess(shape=shape, buffer=num_steps)
+        source.out_ports.s_out.connect(sink.in_ports.a_in)
+
+        run_condition = RunSteps(num_steps=num_steps)
+        run_config = TestRunConfig(select_tag='floating_pt')
+        sink.run(condition=run_condition, run_cfg=run_config)
+        output = sink.data.get()
+        sink.stop()
+
+        self.assertTrue(
+            np.all(output == input),
+            f'Input and Ouptut do not match.\n'
+            f'{output[output!=input]=}\n'
+            f'{input[output!=input] =}\n'
+        )
