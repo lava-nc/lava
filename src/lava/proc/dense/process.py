bc0b95414c11cb10e36b66764d5cb2f9e93caf36
drager-intel <danielle.rager@intel.com>
2021-11-26 15:55:32 -0800

    Cleaned docstrings and comments in Dense Process
    Signed-off-by: drager-intel <danielle.rager@intel.com>

--- src/lava/proc/dense/process.py
+++ src/lava/proc/dense/process.py
@@ -10,37 +10,45 @@ from lava.magma.core.process.ports.ports import InPort, OutPort
 class Dense(AbstractProcess):
     """Dense connections between neurons. Realizes the following abstract
     behavior: a_out = weights * s_in '
+
     Parameters ----------
+
     weights:
     Connection weight matrix.
 
     weight_exp:
     Shared weight exponent used to
     scale magnitude of weights, if needed. Mostly for fixed point
-    implementations. Unnecessary for floating point
-    implementations.
+    implementations. Unnecessary for floating point implementations.
+    Default value is 0.
 
     num_weight_bits:
     Shared weight width/precision used by weight. Mostly for
     fixed point  implementations. Unnecessary for floating point
     implementations.
+    Default is for weights to use full 8 bit precision.
 
     sign_mode:
     Shared indicator whether synapse is of 'null' (0),
-    ’mixed’ (1), ’excitatory’ (2) or  ’inhibitory’ (3) type. If ’mixed’,
-    the sign of the weight is included in the weight bits and
-    the fixed point weight used for inference is scaled by 2. Unnecessary for floating point implementations.
+    ’mixed’ (1, default), ’excitatory’ (2) or  ’inhibitory’ (3) type. If
+    ’mixed’, the sign of the weight is included in the weight bits and
+    the fixed point weight used for inference is scaled by 2.
+    Unnecessary for floating point implementations.
 
-    In the fixed point implementation, weights are scaled according to
+    In the fixed point implementation, weights are scaled according to the
+    following equations:
     weights = weights * (2 ** w_scale)
     w_scale =  8 - num_weight_bits + weight_exp + isMixed()
 
-    s_buff: 1 timestep spike buffer that ensures the process accumulates the
-    spikes of the previous (and not current) timestep. This logic prevents
-    deadlocking.
+    a_buff:
+    1 timestep buffer on the dendritic accumulator that ensures the
+    process sends a_out of the previous (and not current)
+    timestep. This prevents deadlocking for recurrent connectivity
+    architectures.
+
     """
-    #ToDo: Implement a ProcModel that supports synaptic delays. s_buff must
-    # then be adjusted to the length of the delay.
+    #ToDo: Implement a ProcModel that supports synaptic delays. a_buff must
+    # then be adjusted to the length of the delay (DR).
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
