62868c8e290532eeb137dec0bb0e9c8e1812e7ac
Marcus G K Williams <marcus.williams@intel.com>
2021-11-29 06:38:16 -0800

    Lava 0.2.0
    Signed-off-by: Marcus G K Williams <marcus.williams@intel.com>

--- build.py
+++ build.py
@@ -13,7 +13,7 @@ use_plugin('pypi:pybuilder_bandit')
 
 name = "lava-nc"
 default_task = ["analyze", "publish"]
-version = "0.1.0"
+version = "0.2.0"
 summary = "A Software Framework for Neuromorphic Computing"
 url = "https://lava-nc.org"
 license = ["BSD-3-Clause", "LGPL-2.1"]
