b3b9687bde25d4eb3315e34ca55a89edfded531e
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2021-11-30 20:41:48 +0100

    Fixing linting security issue with urllib

--- src/lava/utils/dataloader/mnist.py
+++ src/lava/utils/dataloader/mnist.py
@@ -47,14 +47,16 @@ class MnistDataset:
             err = None
             for mirror in MnistDataset.mirrors:
                 try:
-                    # Disabling security linter and using hardcoded
-                    # URLs specified above
                     url = f"{mirror}{file}"
-                    assert url.lower().startswith("http")
-                    res = urllib.request.urlopen(url)  # nosec
-                    with open(os.path.join(path, file), "wb") as f:
-                        f.write(res.read())
-                    break
+                    if url.lower().startswith("http"):
+                        # Disabling security linter and using hardcoded
+                        # URLs specified above
+                        res = urllib.request.urlopen(url)  # nosec
+                        with open(os.path.join(path, file), "wb") as f:
+                            f.write(res.read())
+                        break
+                    else:
+                        raise "Url does not start with http"
                 except urllib.error.URLError as exception:
                     err = exception
                     continue
