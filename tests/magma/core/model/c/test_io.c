78fbce53bf0dac77e787d68a1260f96f63ecb067
jlakness-intel <84150418+jlakness-intel@users.noreply.github.com>
2021-12-07 14:44:40 -0800

    debugs

--- tests/magma/core/model/c/test_io.c
+++ tests/magma/core/model/c/test_io.c
@@ -4,11 +4,16 @@
 #include "ports.h"
 
 void run(runState* rs){
-    Port *in = get_port("in_port");
-    Port *out = get_port("out_port");
+    printf("run called with phase: %d\n",rs->phase);
+    Port *port = get_port("port");
+    printf("got port %p\n",port);
     int *buf;
-    while(!peek(in))sleep(1);
-    recv(in,&buf);
-    while(!probe(out))sleep(1);
-    send(out,*buf,1);
+    while(!peek(port))sleep(1);
+    printf("ready to recieve at %p\n",&buf);
+    recv(port,&buf);
+    printf("recieved: %p\n",buf);
+    while(!probe(port))sleep(1);
+    printf("ready\n");
+    send(port,buf,1);
+    printf("sent\n");
 }
