0a7362dfd366cf90e1ab1237a1db9d232affe0fd
jlakness-intel <84150418+jlakness-intel@users.noreply.github.com>
2021-12-09 18:05:43 -0800

    remove printf debugs

--- src/lava/magma/core/model/c/ports_python.c
+++ src/lava/magma/core/model/c/ports_python.c
@@ -30,11 +30,8 @@ PyObject* get_port(const char* name){
 
 size_t send(PyObject *port, void *data, size_t m){ // send via pointer
     init_numpy(); 
-    printf("send called on port %p with data %p\n",port,data);
     PyObject *pyDataObj = PyArray_New(&PyArray_Type, 1, (npy_intp[]){m},NPY_INT, NULL,data,0, NPY_ARRAY_CARRAY, NULL);
-    printf("array object created: %p\n",pyDataObj);
     PyObject* result = PyObject_CallMethod(port,"send","O",pyDataObj); // call python port method directly
-    printf("result object: %p\n",result);
     assert(PyLong_Check(result));
     size_t n  = PyLong_AsSize_t((PyLongObject*)result); 
     Py_DECREF(pyDataObj);
@@ -44,18 +41,12 @@ size_t send(PyObject *port, void *data, size_t m){ // send via pointer
 
 size_t recv(PyObject *port,void** data){ // recieve pointer to pointer
     init_numpy(); 
-    printf("calling recv on: %p , to: %p\n",(void*)port,data);
     PyObject *pyDataObj = PyObject_CallMethod(port,"recv",NULL); // call python port object method
-    printf("recv called, got: %p\n",(void*)pyDataObj);
     int dtype = PyArray_DESCR(pyDataObj)->type_num;
     size_t n  = (size_t) PyArray_Size(pyDataObj);
-    printf("got type: %d of size: %d\n",dtype,n);
     PyObject *arrayObj = PyArray_ContiguousFromObject(pyDataObj,dtype,0,0);
-    printf("array object created: %p\n",(void*)arrayObj);
     void *ptr = PyArray_DATA(arrayObj); // get pointer from array
-    printf("pointer retrieved: %p\n",ptr);
     *data = ptr;
-    printf("pointer assigned: %p\n",(void*)*data);  
     Py_DECREF(arrayObj); // WARNING: Not sure what to do about this. Possible deallocated pointer or leave in for possible memory leak 
     Py_DECREF(pyDataObj);
     return n; 
