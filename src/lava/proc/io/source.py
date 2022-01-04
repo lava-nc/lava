1c6877eb858c7281da5e4234319ebd4a865f587d
bamsumit <bam_sumit@hotmail.com>
2022-01-04 12:27:10 -0800

    flake-8 fix

--- src/lava/proc/io/source.py
+++ src/lava/proc/io/source.py
@@ -6,14 +6,14 @@ import numpy as np
 
 from lava.magma.core.process.variable import Var
 from lava.magma.core.process.process import AbstractProcess
-from lava.magma.core.process.ports.ports import OutPort, RefPort
+from lava.magma.core.process.ports.ports import OutPort
 
 from lava.magma.core.resources import CPU
 from lava.magma.core.decorator import implements, requires, tag
 from lava.magma.core.model.py.model import PyLoihiProcessModel
 from lava.magma.core.sync.protocols.loihi_protocol import LoihiProtocol
 from lava.magma.core.model.py.type import LavaPyType
-from lava.magma.core.model.py.ports import PyOutPort, PyRefPort
+from lava.magma.core.model.py.ports import PyOutPort
 
 
 # Ring Buffer #################################################################
