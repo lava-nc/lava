b746bb7806f3d7c51ce00aa9274ffc48e3d1363b
PhilippPlank <32519998+PhilippPlank@users.noreply.github.com>
2022-03-02 18:20:10 +0100

    RefPort's sometimes handled a time step late (#205)
    * - Introducing of a wait() method for RefPorts makes sure that sent data is serviced before the process finishes (which blocks advancing a time step and ensures all VarPorts on the receiving process will be serviced beforehand).
    
    * - optimized Reset by not using a read() to get the data shape
    - added wait() to the dataloader
    - modified docstring of wait
    
    * - modified docstring of wait

--- src/lava/magma/core/model/py/ports.py
+++ src/lava/magma/core/model/py/ports.py
@@ -403,7 +403,9 @@ class PyRefPort(AbstractPyPort):
     CSP ports to a PyVarPort. One channel is used to send data from the
     PyRefPort to the PyVarPort and the other channel is used to receive data
     from the PyVarPort. PyRefPorts can get the value of a referenced Var
-    (read()) or set the value of a referenced Var (write()).
+    (read()), set the value of a referenced Var (write()) and block execution
+    until receipt of prior 'write' commands (sent from PyRefPort to PyVarPort)
+    have been acknowledged (wait()).
 
     Parameters
     ----------
@@ -515,6 +517,25 @@ class PyRefPort(AbstractPyPort):
             The new value of the referenced Var.
         """
         pass
+
+    # TODO: (PP) This should be optimized by a proper CSPSendPort wait
+    def wait(self):
+        """Blocks execution until receipt of prior 'write' commands (sent from
+         RefPort to VarPort) have been acknowledged. Calling wait() ensures that
+         the value written by the RefPort can be received (and set) by the
+         VarPort at the same time step. If wait() is not called, it is possible
+         that the value is received only at the next time step
+         (non-deterministic).
+
+         >>> port = PyRefPort()
+         >>> port.write(5)
+         >>> # potentially do other stuff
+         >>> port.wait()  # waits until (all) previous writes have finished
+
+         Preliminary implementation. Currently, a simple read() ensures the
+         writes have been acknowledged. This is inefficient and will be
+         optimized later at the CspChannel level"""
+        self.read()
 
     def _transform(self, recv_data: np.array) -> np.array:
         """Applies all transformation function pointers to the input data.
