1e1474b42c2be830ca49b0e744585bea9c4b08ed
Mathis Richter <mathis.richter@intel.com>
2022-03-02 15:33:39 +0100

    Virtual ports between RefPorts and VarPorts (#195)
    * permute initial implementation
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Tests for permute ports
    
    * Process property of virtual ports no longer returns None
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * Added initial run-unittest for flatten() from issue #163
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * User-level API for TransposePort with unit tests
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * Fixed typo
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * Unit tests for flatten() and concat_with()
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * Unit tests for virtual ports in Processes that are executed (wip)
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * Preliminary implementation of virtual ports between OutPort and InPort (wip)
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * Fixing unit tests after merge
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * Added support for virtual ports between an OutPort and InPort of two hierarchical Processes
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * Clean up, exceptions, and generic unit tests for virtual port topologies
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * Fixed linter issues
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * Raising an exception when executing ConcatPort
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * Unit tests for virtual ports between OutPorts and InPorts in hierarchical Processes.
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * RefPort writing to an explicit VarPort via a virtual port.
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * RefPort reading from an explicit VarPort via a virtual port.
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * Fixed linter error
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * Unit tests for virtual ports between RefPorts and VarPorts in hierarchical Processes.
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * Fixed a docstring
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    * Added docstrings to methods get_transform_func_fwd/bwd.
    
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>
    
    Co-authored-by: bamsumit <bam_sumit@hotmail.com>
    Co-authored-by: Marcus G K Williams <168222+mgkwill@users.noreply.github.com>

--- src/lava/magma/core/model/py/ports.py
+++ src/lava/magma/core/model/py/ports.py
@@ -460,7 +460,10 @@ class PyRefPort(AbstractPyPort):
                  csp_recv_port: ty.Optional[CspRecvPort],
                  process_model: AbstractProcessModel,
                  shape: ty.Tuple[int, ...] = tuple(),
-                 d_type: type = int):
+                 d_type: type = int,
+                 transform_funcs: ty.Optional[ty.List[ft.partial]] = None):
+
+        self._transform_funcs = transform_funcs
         self._csp_recv_port = csp_recv_port
         self._csp_send_port = csp_send_port
         super().__init__(process_model, shape, d_type)
@@ -513,6 +516,25 @@ class PyRefPort(AbstractPyPort):
         """
         pass
 
+    def _transform(self, recv_data: np.array) -> np.array:
+        """Applies all transformation function pointers to the input data.
+
+        Parameters
+        ----------
+        recv_data : numpy.ndarray
+            data received on the port that shall be transformed
+
+        Returns
+        -------
+        recv_data : numpy.ndarray
+            received data, transformed by the incoming virtual ports
+        """
+        if self._transform_funcs:
+            # apply all transformation functions to the received data
+            for f in reversed(self._transform_funcs):
+                recv_data = f(recv_data)
+        return recv_data
+
 
 class PyRefPortVectorDense(PyRefPort):
     """Python implementation of RefPort for dense vector data."""
@@ -529,8 +551,10 @@ class PyRefPortVectorDense(PyRefPort):
             header = np.ones(self._csp_send_port.shape) * VarPortCmd.GET
             self._csp_send_port.send(header)
 
-            return self._csp_recv_port.recv()
+            return self._transform(self._csp_recv_port.recv())
 
+        # TODO (MR): self._shape must be set to the correct shape when
+        #  instantiating the Port
         return np.zeros(self._shape, self._d_type)
 
     def write(self, data: np.ndarray):
@@ -660,7 +684,10 @@ class PyVarPort(AbstractPyPort):
                  csp_recv_port: ty.Optional[CspRecvPort],
                  process_model: AbstractProcessModel,
                  shape: ty.Tuple[int, ...] = tuple(),
-                 d_type: type = int):
+                 d_type: type = int,
+                 transform_funcs: ty.Optional[ty.List[ft.partial]] = None):
+
+        self._transform_funcs = transform_funcs
         self._csp_recv_port = csp_recv_port
         self._csp_send_port = csp_send_port
         self.var_name = var_name
@@ -692,6 +719,25 @@ class PyVarPort(AbstractPyPort):
         """
         pass
 
+    def _transform(self, recv_data: np.array) -> np.array:
+        """Applies all transformation function pointers to the input data.
+
+        Parameters
+        ----------
+        recv_data : numpy.ndarray
+            data received on the port that shall be transformed
+
+        Returns
+        -------
+        recv_data : numpy.ndarray
+            received data, transformed by the incoming virtual ports
+        """
+        if self._transform_funcs:
+            # apply all transformation functions to the received data
+            for f in self._transform_funcs:
+                recv_data = f(recv_data)
+        return recv_data
+
 
 class PyVarPortVectorDense(PyVarPort):
     """Python implementation of VarPort for dense vector data."""
@@ -712,7 +758,7 @@ class PyVarPortVectorDense(PyVarPort):
 
                 # Set the value of the Var with the given data
                 if enum_equal(cmd, VarPortCmd.SET):
-                    data = self._csp_recv_port.recv()
+                    data = self._transform(self._csp_recv_port.recv())
                     setattr(self._process_model, self.var_name, data)
                 elif enum_equal(cmd, VarPortCmd.GET):
                     data = getattr(self._process_model, self.var_name)
