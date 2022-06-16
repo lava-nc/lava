b3970800263a4cafdcd07024a0e7f83047877f7e
weidel-p <philipp.weidel@intel.com>
2022-06-16 18:23:19 +0200

    used magic to export single cells instead of complete script

--- src/lava/utils/win_exec.py
+++ src/lava/utils/win_exec.py
@@ -3,37 +3,30 @@
 # See: https://spdx.org/licenses/
 
 from sys import platform
-import os
 
 
-def export_notebook(nb_name: str):
-    """Enables execution of Lava tutorials on Windows systems. Processes
-    defined within the tutorial jupyter notebook need to be written to a python
-    script and imported in the notebook again."""
+try:
+    from IPython import get_ipython
+except ModuleNotFoundError:
+    pass
+
+def export(filename, cell):
+    '''Exports the cell to the given file name if system is windows.'''
     if platform == "win32" or platform == "cygwin":
-        # Convert the ipython notebook to a python script
-        os.system("jupyter nbconvert --to script " + nb_name + ".ipynb")
-
-        # Remove code after definition to avoid execution during import
-        with open(nb_name + ".py", "r+") as f:
-            d = f.readlines()
-            f.seek(0)
-            for i in d:
-                if i.strip("\n") != "# #### Exception for Windows":
-                    f.write(i)
-                elif i.strip("\n") == "# #### Exception for Windows":
-                    break
-            f.truncate()
-
-
-def cleanup(nb_name: str):
-    """Removes previously created python script for tutorial execution on
-    Windows systems."""
-    if platform == "win32" or platform == "cygwin":
-        os.system("del " + nb_name + ".py")
+        with open(f"{filename}.py", "a") as f:
+            f.write(cell)
     else:
-        os.remove(nb_name + ".py")
+        get_ipython().ex(cell)
 
+
+def load_ipython_extension(shell):
+    '''Registers the magic function when the extension loads.'''
+    shell.register_magic_function(export, 'cell')
+
+
+def unload_ipython_extension(shell):
+    '''Unregisters the magic function when the extension unloads.'''
+    del shell.magics_manager.magics['cell']['export']
 
 if __name__ == "__main__":
     pass
