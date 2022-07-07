d79ddfe74ff1d9bfb32be06026898dd86ee1ad7b
kds300 <kds300@psu.edu>
2022-07-07 14:50:45 -0400

    added license information

--- src/lava/proc/delay/models.py
+++ src/lava/proc/delay/models.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+# See: https://spdx.org/licenses/
+
 # Modified from src/lava/proc/dense/process.py: PyDenseModelFloat
 # Modified by Kevin Sargent, Pennsylvania State University 
 
