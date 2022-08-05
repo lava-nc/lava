789caf07d4c08a950418add3611db699ee2739dc
hexu1 <he.xu@intel.com>
2022-08-05 12:52:13 +0800

    Follow Google coding style for test/test_example.py file

--- src/lava/magma/runtime/cpp_message_infrastructure/test/test_example.py
+++ src/lava/magma/runtime/cpp_message_infrastructure/test/test_example.py
@@ -5,14 +5,18 @@
 from PyWrapper import MultiProcessing
 import time
 
+
 def print_hello():
     print("hello")
+
+
 def main():
     mp = MultiProcessing()
     for i in range(5):
         mp.build_actor(print_hello)
 
     mp.check_actor()
+
 
 main()
 print("sleep 5")
