cd50a84c6b3c6d7ac68c0feaad5bd264081ec0a4
shaline-koh <shaline.jia.thean.koh@intel.com>
2022-08-15 16:03:37 +0800

    Added tranformer header file

---
+++ src/lava/magma/runtime/message_infrastructure/message_infrastructure/csrc/transformer.h
@@ -0,0 +1,35 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: BSD-3-Clause
+// See: https://spdx.org/licenses/
+
+#ifndef TRANSFORMER_H_
+#define TRANSFORMER_H_
+
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+
+#include <vector>
+
+#include "abstract_port_implementation.h"
+
+namespace message_infrastructure {
+
+class AbstractTransformer{
+ public:
+    virtual std::vector<pybind11::array_t<pybind11::dtype>> Transform();
+};
+
+class IdentityTransformer: public AbstractTransformer {
+ public:
+    std::vector<pybind11::array_t<pybind11::dtype>> Transform();
+};
+
+class VirtualPortTransformer: public AbstractTransformer {
+ public:
+    std::vector<pybind11::array_t<pybind11::dtype>> Transform();
+    std::vector<pybind11::array_t<pybind11::dtype>> _Get_Transform();
+};
+
+}  // namespace message_infrastructure
+
+#endif  // TRANSFORMER_H_
