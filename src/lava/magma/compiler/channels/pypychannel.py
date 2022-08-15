967f707557621e2d7707d720b6c9af7749d58453
Miao, Hongda <hongda.miao@intel.com>
2022-08-15 19:01:52 +0800

    Fix package path error

--- src/lava/magma/compiler/channels/pypychannel.py
+++ src/lava/magma/compiler/channels/pypychannel.py
@@ -17,7 +17,8 @@ from lava.magma.compiler.channels.interfaces import (
 
 if ty.TYPE_CHECKING:
     from lava.magma.runtime.message_infrastructure \
-        .message_infrastructure_interface import (
+        .message_infrastructure.message_infrastructure_interface \
+        import (
             MessageInfrastructureInterface)
 
 
