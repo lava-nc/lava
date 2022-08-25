916b799b4c128c8c9ba425b7a1494553695c0d27
Philipp <philipp.weidel@intel.com>
2022-08-25 04:30:25 -0700

    rm init file

--- src/lava/proc/io/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2021-22 Intel Corporation
-# SPDX-License-Identifier: BSD-3-Clause
-# See: https://spdx.org/licenses/
-
-from . import reset, source, sink, dataloader
-
-__all__ = ['reset', 'source', 'sink', 'dataloader']
