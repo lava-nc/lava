5627285926804394d54f26b710f6184115e914ed
Seow, Wen Jie <wen.jie.seow@intel.com>
2022-08-30 10:12:28 +0800

    Write test cases for multiprocessing

--- src/lava/magma/runtime/message_infrastructure/test/test_multiprocessing.py
+++ src/lava/magma/runtime/message_infrastructure/test/test_multiprocessing.py
@@ -3,6 +3,8 @@
 # See: https://spdx.org/licenses/
 import traceback
 import unittest
+import psutil
+import os
 from functools import partial
 
 from message_infrastructure import CppMultiProcessing
@@ -37,36 +39,77 @@ def target_fn(*args, **kwargs):
 
 
 class TestMultiprocessing(unittest.TestCase):
+    mp = MultiProcessing()
+
     def test_multiprocessing_spawn(self):
         """
         Spawns an actor.
         Checks that an actor is spawned successfully.
         """
-        pass
+        self.mp.start()
+        builder = Builder()
+        return_type = self.mp.build_actor(target_fn, builder, idx=1)
+        # build_actor returns 0 if it is a child process
+        self.assertEqual(return_type, 0)
+
+        self.test_get_actor_pid()
 
     def test_multiprocessing_shutdown(self):
         """
         Spawns an actor and sends a stop signal.
         Checks that actor is stopped successfully.
         """
-        pass
+        self.test_multiprocessing_spawn()
 
+        actor_list = self.mp.actor_pids()
+        self.mp.stop()
+
+        # Check that all actor PIDs no longer exist
+        for actor_pid in actor_list:
+            self.assertFalse(psutil.pid_exists(actor_pid))
+
+    @unittest.skip("Test case fir reassigning actors to Processes not yet \
+        implemented")
     def test_multiprocessing_kill(self):
         """
         Spawns an actor and kills it after a certain time.
         Checks that multiprocessing daemon will automatically restart crashed
         actor and reassign to correct process.
         """
-        pass
+        self.test_multiprocessing_spawn()
+
+        # Gets list of actor PIDs and kills the 1st one in the list
+        actor_list = self.mp.actor_pids()
+        os.kill(actor_list[0])
+
+        # TODO: How to check that an actor has been reassinged to the correct
+        # process
 
     def test_get_actor_pid(self):
-        pass
+        """
+        Gets list of actor PIDs
+        Checks that all actor PIDs exist
+        """
+        actor_list = self.mp.actor_pids()
+        for actor_pid in actor_list:
+            self.assertTrue(psutil.pid_exists(actor_pid))
 
     def test_get_actor_list(self):
-        pass
+        """
+        Gets list of actors
+        Checks that all actors are of Actor type
+        """
+        actor_list = self.mp.actors()
+        for actor in actor_list:
+            self.assertIsInstance(actor, Actor)
 
     def test_get_shared_memory_manager(self):
-        pass
+        """
+        Gets the Shared Memory Manager
+        Checks that the shared memory manager is of SharedMemManager type
+        """
+        shared_memory_manager = self.mp.smm()
+        self.assertIsInstance(shared_memory_manager, SharedMemManager)
 
 
 def test_multiprocessing():
