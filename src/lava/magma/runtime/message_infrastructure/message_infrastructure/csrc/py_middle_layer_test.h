2ac4eaca6e06ab7f40f30233097292f7558315ea
Miao, Hongda <hongda.miao@intel.com>
2022-09-06 17:31:33 +0800

    update message toml, fix cpplint, set back lava ci

--- src/lava/magma/runtime/message_infrastructure/message_infrastructure/csrc/py_middle_layer_test.h
+++ src/lava/magma/runtime/message_infrastructure/message_infrastructure/csrc/py_middle_layer_test.h
@@ -6,9 +6,12 @@
 #define PY_MIDDLE_LAYER_TEST_H_
 
 #include "py_middle_layer.h"
-#include "message_infrastructure_logging.h"
-#include <stdlib.h>
+
 #include <iostream>
+
+#include "message_infrastructure_logging.h"
+
+
 namespace message_infrastructure {
 
 class SimplePort {
@@ -36,15 +39,17 @@ class SimplePort {
     LAVA_LOG(LOG_MP, "dim[0]:%d\n", this->metadata->dimensions[0]);
     // if (metadata->type == 'i') {
     if (1) {
-      int *data = (int*) this->metadata->mdata;
-      for (int i=0; i<this->metadata->dimensions[0]*this->metadata->strides[0]/sizeof(int); i++) {
-        data[i] ++;
+      int *data = reinterpret_cast<int*> (this->metadata->mdata);
+      for (int i = 0; i < this->metadata->dimensions[0] \
+                    *this->metadata->strides[0]/sizeof(int); i++) {
+        data[i]++;
       }
     }
     LAVA_LOG(LOG_MP, "transfer achieved\n");
   }
+
  private:
-  MetaData *metadata=nullptr;
+  MetaData *metadata = nullptr;
 };
 
 class ProxySimplePort {
@@ -55,30 +60,28 @@ class ProxySimplePort {
   py::object* set_data(py::object *obj) {
     LAVA_LOG(LOG_MP, "set data\n");
     MetaData *metadata = new MetaData();
-    //MDataFromObject(metadata, obj);
+    // MDataFromObject(metadata, obj);
     this->obj = obj;
-    //port->set_data(metadata);
-    std::cout << (long)this->obj << std::endl;
+    // port->set_data(metadata);
+    std::cout << (uint64_t)this->obj << std::endl;
     return this->obj;
   }
   py::object* get_data() {
     LAVA_LOG(LOG_MP, "get data\n");
-    //MetaData *metadata = port->get_data();
-    //MDataToObject(metadata, this->obj);
-    std::cout << (long)this->obj << std::endl;
+    // MetaData *metadata = port->get_data();
+    // MDataToObject(metadata, this->obj);
+    std::cout << (uint64_t)this->obj << std::endl;
     return this->obj;
   }
-  void transfer(){
+  void transfer() {
     port->transfer();
   }
+
  private:
   py::object *obj;
   SimplePort *port;
 };
 
-
-
-
 }  // namespace message_infrastructure
 
-#endif  // PY_MIDDLE_LAYER_H_
+#endif  // PY_MIDDLE_LAYER_TEST_H_
