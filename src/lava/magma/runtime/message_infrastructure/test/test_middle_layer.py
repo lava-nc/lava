f2f7f83e1820da01c7f52636943be5d6c2da57eb
Miao, Hongda <hongda.miao@intel.com>
2022-09-07 20:57:34 +0800

    fix cpplint and update the worked test

--- src/lava/magma/runtime/message_infrastructure/test/test_middle_layer.py
+++ src/lava/magma/runtime/message_infrastructure/test/test_middle_layer.py
@@ -5,9 +5,11 @@ import numpy as np
 def main():
     pter = PyDataTransfer()
     data = np.array([1, 2, 3], dtype=np.int32)
-    pter.mdata_from_object(data)
+    pter.set_obj(data)
+    pter.change_var()
 
     x = pter.get_obj()
     print(x)
+    print(data)
 
 main()
