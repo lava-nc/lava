bde59ebc0e450c8f621706feef10b013abb6be81
GaboFGuerra <83413252+GaboFGuerra@users.noreply.github.com>
2022-09-27 23:53:48 +0200

    Update ReadGate API according to NC model. (#384)
    * Enable exception proc_map, working with dataclasses, etc.
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    
    * Integrate YS's changes to enable req_pause.
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    
    * Expose noise amplitude at the process level for SCIF.
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    
    * Remove prints from req_pause debug.
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    
    * Update ReadGate API.
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    
    * Update API on the Python model.
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    
    * Solve lint issue.
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>

--- src/lava/proc/read_gate/process.py
+++ src/lava/proc/read_gate/process.py
@@ -13,32 +13,26 @@ class ReadGate(AbstractProcess):
 
     Parameters
     ----------
-    shape : tuple(int)
-        The shape of the set of units in the downstream process whose state
+    shape: The shape of the set of units in the downstream process whose state
         will be read by ReadGate.
-    target_cost : float, optional
-        cost value at which, once attained by the network,
+    target_cost: cost value at which, once attained by the network,
         this process will stop execution.
-    name : str
-        Name of the Process. Default is 'Process_ID', where ID is an
+    name: Name of the Process. Default is 'Process_ID', where ID is an
         integer value that is determined automatically.
-    log_config : LogConfig
-        Configuration options for logging.
+    log_config: Configuration options for logging.
 
     InPorts
     -------
-    cost_in
-        Receives a better cost found by the CostIntegrator at the
+    cost_in: Receives a better cost found by the CostIntegrator at the
         previous timestep.
 
     OutPorts
     --------
-    cost_out
-        Forwards to an upstream process the better cost notified by the
+    cost_out: Forwards to an upstream process the better cost notified by the
         CostIntegrator.
-    solution_out
-        Forwards to an upstream process the better variable assignment
+    solution_out: Forwards to an upstream process the better variable assignment
         found by the solver network.
+    send_pause_request: Notifies upstream process to request execution to pause.
     """
 
     def __init__(self,
@@ -53,5 +47,6 @@ class ReadGate(AbstractProcess):
         self.target_cost = Var(shape=(1,), init=target_cost)
         self.cost_in = InPort(shape=(1,))
         self.cost_out = OutPort(shape=(1,))
+        self.send_pause_request = OutPort(shape=(1,))
         self.solution_out = OutPort(shape=shape)
         self.solution_reader = RefPort(shape=shape)
