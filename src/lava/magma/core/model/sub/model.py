15c2574f805ffff0d8b8dd3fbf99973b66909057
GaboFGuerra <83413252+GaboFGuerra@users.noreply.github.com>
2022-09-27 22:08:46 +0200

    Enable exception proc_map, working with dataclasses, etc. (#372)
    * Enable exception proc_map, working with dataclasses, etc.
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    
    * Fix lint issue and check if run_cfg has exception model map
    
    Signed-off-by: GaboFGuerra <gabriel.fonseca.guerra@intel.com>
    Co-authored-by: Marcus G K Williams <168222+mgkwill@users.noreply.github.com>
    Co-authored-by: Joyesh Mishra <joyesh.mishra@intel.com>

--- src/lava/magma/core/model/sub/model.py
+++ src/lava/magma/core/model/sub/model.py
@@ -8,6 +8,7 @@ from collections import OrderedDict
 
 from lava.magma.core.model.model import AbstractProcessModel
 from lava.magma.core.process.process import AbstractProcess
+from dataclasses import is_dataclass, fields
 
 
 class AbstractSubProcessModel(AbstractProcessModel):
@@ -58,6 +59,12 @@ class AbstractSubProcessModel(AbstractProcessModel):
         procs = OrderedDict()
         for attr_name in dir(self):
             attr = getattr(self, attr_name)
-            if isinstance(attr, AbstractProcess):
+            if isinstance(attr, AbstractProcess) and \
+                    attr is not self.implements_process:
                 procs[attr_name] = attr
+            if is_dataclass(attr):
+                for data in fields(attr):
+                    sub_attr = getattr(attr, data.name)
+                    if isinstance(sub_attr, AbstractProcess):
+                        procs[type(sub_attr).__name__] = sub_attr
         return procs
