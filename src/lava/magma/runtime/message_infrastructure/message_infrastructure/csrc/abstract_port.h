4965e4625f649272a103f23706c1cd14ffef17ee
luoxiaoc <xiaochuan.luo@intel.com>
2022-09-30 14:12:30 +0800

    clean codes
    Signed-off-by: luoxiaoc <xiaochuan.luo@intel.com>

--- src/lava/magma/runtime/message_infrastructure/message_infrastructure/csrc/abstract_port.h
+++ src/lava/magma/runtime/message_infrastructure/message_infrastructure/csrc/abstract_port.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2021 Intel Corporation
+// Copyright (C) 2022 Intel Corporation
 // SPDX-License-Identifier: BSD-3-Clause
 // See: https://spdx.org/licenses/
 
@@ -12,6 +12,7 @@
 #include "utils.h"
 
 namespace message_infrastructure {
+
 class AbstractPort {
  public:
   AbstractPort(const std::string &name, const size_t &size,
