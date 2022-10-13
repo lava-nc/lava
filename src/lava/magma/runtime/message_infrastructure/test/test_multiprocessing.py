d8b82c3d4b98890753970c44f80cbda13eaba857
Miao, Hongda <hongda.miao@intel.com>
2022-10-13 10:57:34 +0800

    recover test_multiprocessing.py

--- src/lava/magma/runtime/message_infrastructure/test/test_multiprocessing.py
+++ src/lava/magma/runtime/message_infrastructure/test/test_multiprocessing.py
@@ -10,9 +10,14 @@ import numpy as np
 from functools import partial
 from enum import Enum
 
+from message_infrastructure import CppMultiProcessing
+from message_infrastructure import ProcessType
 from message_infrastructure import Actor
 from message_infrastructure import ActorStatus
 from message_infrastructure.multiprocessing import MultiProcessing
+from message_infrastructure import SendPort
+from message_infrastructure import RecvPort
+from message_infrastructure import Channel
 
 import time
 
@@ -46,124 +51,20 @@ def target_fn(*args, **kwargs):
         print(traceback.format_exc())
         raise e
 
-def multiprocessing_spawn(builder, target_fn):
-        """
-        Spawns an actor.
-        """
+
+class TestMultiprocessing(unittest.TestCase):
+
+    def test_multiprocessing_actors(self):
         mp = MultiProcessing()
         mp.start()
-        #builder = Builder()
-        # Build 5 actors
+        builder = Builder()
         for i in range(5):
             bound_target_fn = partial(target_fn, idx=i)
-            mp.build_actor(bound_target_fn, builder)
+            ret = mp.build_actor(bound_target_fn, builder)
 
-        return mp, mp.actors
-class TestMultiprocessing(unittest.TestCase):
-    
-    @unittest.skip
-    def test_multiprocessing_spawn(self):
-        """
-        Spawns an actor.
-        Checks that an actor is spawned successfully.
-        """
-        self.builder = Builder()
-
-        self.mp, self.actor_list = multiprocessing_spawn(self.builder, target_fn)
-        
-        time.sleep(5)
-
-        for actor in self.actor_list:
-            print("status_run: ", actor.get_status())
-            #self.assertEqual(actor.get_status(), 0)
-
-        self.mp.stop(True)
-
-    #@unittest.skip
-    def test_multiprocessing_pause(self):
-        builder = Builder()
-        self.mp, self.actor_list = multiprocessing_spawn(builder, target_fn)
-        
-        time.sleep(5)
-        #self.mp.pause()
-
-        for actor in self.actor_list:
-            actor.pause()
-            time.sleep(1)
-            print("status_pause: ", actor.get_status())
-            #self.assertEqual(actor.get_status(), 1)
-
-        self.mp.stop(True)
-
-    @unittest.skip 
-    def test_multiprocessing_shutdown(self):
-        """
-        Spawns an actor and sends a stop signal.
-        Checks that actor is stopped successfully.
-        """
-
-        self.builder = Builder()
-        self.mp, self.actor_list = multiprocessing_spawn(self.builder, target_fn)
-        
-        time.sleep(5)
-        
-        for actor in self.actor_list:
-            actor.stop()
-            time.sleep(1)
-            print("status_stop: ", actor.get_status())
-            #self.assertEqual(actor.get_status(), 2)
-
-        self.mp.stop(True)
-
-        # actor_list = self.mp.actor_pids
-        # self.mp.stop()
-
-        # Check that all actor PIDs no longer exist
-        # for actor_pid in actor_list:
-        #     self.assertFalse(psutil.pid_exists(actor_pid))
-   
-    @unittest.skip
-    def test_get_actor_list(self):
-        """
-        Gets list of actors
-        Checks that all actors are of Actor type
-        """
-
-        self.builder = Builder()
-        self.mp, self.actor_list = multiprocessing_spawn(self.builder, target_fn)
-        time.sleep(5)
-        for actor in self.actor_list:
-            self.assertIsInstance(actor, Actor)
-
-        self.mp.stop(True)
-
-def test_multiprocessing():
-    mp = MultiProcessing()
-    mp.start()
-    builder = Builder()
-    for i in range(5):
-        bound_target_fn = partial(target_fn, idx=i)
-        ret = mp.build_actor(bound_target_fn, builder)
-        #print(ret)
-    time.sleep(5)
-    # shmm = mp.smm
-    # for i in range(5):
-    #     print("shared memory id: ", shmm.alloc_mem(8))
-
-    actors = mp.actors
-    for actor in actors:
-        actor = actors[0]
-        print("actor status0: ", actor.get_status())
-        time.sleep(5)
-        for actor in actors:
-            actor.stop()
-            print("actor status1: ", actor.get_status())
+        time.sleep(0.1)
         mp.stop(True)
 
-    # print("stop num: ", shmm.stop())
-    # print("stop num: ", shmm.stop())
 
-# Run unit tests
 if __name__ == '__main__':
-    #test_multiprocessing()
     unittest.main()
