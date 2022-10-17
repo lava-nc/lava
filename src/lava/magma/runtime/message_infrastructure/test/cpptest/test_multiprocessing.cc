4a14fa85091b79128c610669fa1ceffb55aa320a
Miao, Hongda <hongda.miao@intel.com>
2022-10-17 10:58:14 +0800

    remove hello test

--- src/lava/magma/runtime/message_infrastructure/test/cpptest/test_multiprocessing.cc
+++ src/lava/magma/runtime/message_infrastructure/test/cpptest/test_multiprocessing.cc
@@ -8,12 +8,6 @@
 #include <multiprocessing.h>
 #include <abstract_actor.h>
 
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
-}
 
 using namespace message_infrastructure;
 
