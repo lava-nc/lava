4d32aed59d2fcf20c7008b91d8ce5bbceb6c542b
Jurado <mjurado3@eosl.gtri.org>
2022-10-18 11:10:30 -0400

    Cleaned up test_resonator_process.py

--- test_resonator_process.py
+++ test_resonator_process.py
@@ -8,8 +8,11 @@ from lava.proc.monitor.process import Monitor
 import matplotlib.pyplot as plt
 
 from lava.proc.io.source import RingBuffer as RingBufferSend
-from lava.proc.io.sink import RingBuffer as RingBufferReceive, PyReceiveModelFloat
+from lava.proc.io.sink import RingBuffer as RingBufferReceive, \
+                                            PyReceiveModelFloat
+
 # from lava.proc.rf_iz.process import RF_IZ
+
 
 def process_spikes(time_range: np.array, spikes: np.array):
     spike_mask = np.argwhere(spikes)
@@ -17,17 +20,22 @@ def process_spikes(time_range: np.array, spikes: np.array):
     spike_amps = spikes[spike_mask]
     return list(zip(toas, spike_amps))
 
-def plot_spike_graph(time_range: np.array, signal: list, spikes: np.array, real_comp: np.array, complex_threshold: int, title:str, save_name:str):
+
+def plot_spike_graph(time_range: np.array, signal: list, spikes: np.array, 
+                     real_comp: np.array, complex_threshold: int,
+                     title: str, save_name: str):
     plt.figure()
     spike_info = process_spikes(time_range, spikes)
 
-    plt.plot(time_range, [complex_threshold] * len(time_range), label ="threshold", color = "black")
+    plt.plot(time_range, [complex_threshold] * len(time_range),
+             label="threshold", color="black")
     for toa, amp in spike_info:
-        plt.vlines(toa,complex_threshold, complex_threshold+amp, color = "black", linestyles="--", label = "spike")
+        plt.vlines(toa, complex_threshold, complex_threshold+amp,
+                   color="black", linestyles="--", label="spike")
 
-    plt.plot(time_range, real_comp.flatten(), label = "Re (z(t))")
+    plt.plot(time_range, real_comp.flatten(), label="Re (z(t))")
 
-    plt.plot(time_range, signal - 2, color = "orange", label = "input pulse")
+    plt.plot(time_range, signal - 2, color="orange", label="input pulse")
     plt.legend()
     plt.title(title)
     plt.xlabel("Time")
@@ -54,25 +62,26 @@ if __name__ == "__main__":
     num_steps = 100
 
     # create lava neuron
-    neuron = RF(shape=(n_neurons,), period = period, alpha = alpha, vth = vth, state_exp=state_exp, decay_bits=decay_bits) # create a single neuron
+    neuron = RF(shape=(n_neurons,), period=period, alpha=alpha,
+                vth=vth, state_exp=state_exp, decay_bits=decay_bits)
 
     # create spiking data that just spike at regular intervals
-    incoming_spike_data = np.zeros((n_neurons, num_steps))  
-    incoming_spike_data[:, [0, 10]] =1  # neuron recieves input spikes at timestep 10, 20, 30, and 40
+    incoming_spike_data = np.zeros((n_neurons, num_steps))
+    incoming_spike_data[:, [0]] = 1
     incoming_data_process = RingBufferSend(data=incoming_spike_data)
-    incoming_data_process.out_ports.s_out.connect(neuron.a_real_in) # connect incoming spikes to neuron
+    incoming_data_process.out_ports.s_out.connect(neuron.a_real_in)
 
     # Create a receptacle for the neurons output spikes
-    outgoing_data_process = RingBufferReceive(shape=(n_neurons,), buffer =num_steps)
+    outgoing_data_process = RingBufferReceive(shape=(n_neurons,),
+                                              buffer=num_steps)
     neuron.s_out.connect(outgoing_data_process.a_in)
 
     # monitor the real component of the neuron
     monitor = Monitor()
-    monitor.probe(target=neuron.real, num_steps=num_steps) # create a probe to observe oscillations
+    monitor.probe(target=neuron.real, num_steps=num_steps)  # observe internals
     if fixed_point:
         class CustomRunConfig(Loihi1SimCfg):
             def select(self, proc, proc_models):
-                # customize run config to always use float model for io.sink.RingBuffer
                 if isinstance(proc, RingBufferReceive):
                     return PyReceiveModelFloat
                 else:
@@ -83,14 +92,23 @@ if __name__ == "__main__":
         run_config = Loihi1SimCfg()
 
     # run our simulation for n_steps
-    neuron.run(condition=RunSteps(num_steps=num_steps, blocking=True), run_cfg=run_config)
+    neuron.run(condition=RunSteps(num_steps=num_steps, blocking=True),
+               run_cfg=run_config)
 
     probe_data_real = monitor.get_data()[neuron.name]["real"]
     probe_data_spike = outgoing_data_process.data.get()
     neuron.stop()  # clears data in probe with stop
     
-    assert n_neurons == 1, "Rest of Logic will not work if there are more than 1 neurons"
+    assert n_neurons == 1
     if fixed_point:
-        plot_spike_graph(np.arange(num_steps), incoming_spike_data.flatten() > 0, probe_data_spike.flatten() > 0, probe_data_real.flatten()/ (1 << 6), vth, "Rf Neuron in Lava Fixed point", "rf_lava.png")
+        plot_spike_graph(np.arange(num_steps),
+                         incoming_spike_data.flatten() > 0,
+                         probe_data_spike.flatten() > 0,
+                         probe_data_real.flatten()/(1 << 6), vth,
+                         "Rf Neuron in Lava Fixed point", "rf_lava.png")
     else:
-        plot_spike_graph(np.arange(num_steps), incoming_spike_data.flatten() > 0, probe_data_spike.flatten() > 0, probe_data_real.flatten(), vth, "Rf Neuron in Lava float point", "rf_lava.png")        
+        plot_spike_graph(np.arange(num_steps),
+                         incoming_spike_data.flatten() > 0,
+                         probe_data_spike.flatten() > 0,
+                         probe_data_real.flatten(), vth,
+                         "Rf Neuron in Lava float point", "rf_lava.png")
