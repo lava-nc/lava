15a3f7d928bada14369866fd291ec4b1cc4177db
Wenhui Ye <wenhui.ye@intel.com>
2022-10-18 15:44:54 +0800

    add 2 options of log
            add: 2 cmake command line arguments to indicate: which levels log would be recorded, and where printed to.
                    - MSG_LOG_LEVEL:
    			which levels log would be recorded, valid values:
                              all: all of levels would be recorded;
                              err_warn: both error and warning would be recorded;
                              a number from 0 to 31, consists of followings:
                            	#define LOG_MASK_NULL (0)
                                    #define LOG_MASK_INFO (1)
                                    #define LOG_MASK_DUMP (1<<1)
                                    #define LOG_MASK_DBUG (1<<2)
                                    #define LOG_MASK_WARN (1<<3)
                                    #define LOG_MASK_ERRO (1<<4)
    			DEFAULT VALUE: 16, error log only.
    		- MSG_LOG_PRINT_MODE:
    			where would log be printed to, valid values:
    			  all: both of console and file;
    			  file: file only;
    			  shell: console only;
    			  null: neither
    			  a number from 0 to 3, consists of followings:
    				#define LOG_PRINT_MASK_NULL (0)
    				#define LOG_PRINT_MASK_SHEL (1)
    				#define LOG_PRINT_MASK_FILE (2)
    			DEFAULT VALUE: 1, console only.

--- src/lava/magma/runtime/message_infrastructure/message_infrastructure/csrc/core/message_infrastructure_logging.cc
+++ src/lava/magma/runtime/message_infrastructure/message_infrastructure/csrc/core/message_infrastructure_logging.cc
@@ -8,111 +8,108 @@ namespace message_infrastructure {
 
 const char* LOG_MODULE_NAME[] = {
   "LOG_MULL_MODULE",
-  "LOG_MPROC", // multiprocess
+  "LOG_MPROC",  // multiprocess
   "LOG_ACTOR",
   "LOG_LAYER",
   "LOG_SHMEM"
 };
 
-LogMsg::LogMsg(const std::string &msg_data, const char* log_file, int log_line, enum Log_Module log_module, const char* log_level){
-    msg_data_ = msg_data;
-    msg_time_ = getTime();
-    msg_file_ = std::string(log_file);
-    msg_level_ = std::string(log_level);
-    msg_module_ = std::string(LOG_MODULE_NAME[log_module]);
-    msg_pid_ = "0";
-    msg_line_ = log_line;
+LogMsg::LogMsg(const std::string &msg_data,
+               const char* log_file,
+               int log_line,
+               enum Log_Module log_module,
+               const char* log_level) {
+  msg_data_ = msg_data;
+  msg_time_ = getTime();
+  msg_file_ = std::string(log_file);
+  msg_level_ = std::string(log_level);
+  msg_module_ = std::string(LOG_MODULE_NAME[log_module]);
+  msg_line_ = log_line;
 }
 
-std::string LogMsg::getEntireLogMsg(int pid){
-    std::stringstream buf;
-    buf << msg_time_    << " ";
-    buf << msg_level_   << " ";
-    buf << pid          << " ";
-    buf << msg_module_  << " ";
-    buf << msg_file_    << ":";
-    buf << msg_line_    << " ";
-    buf << msg_data_;
-    return buf.str();
+std::string LogMsg::getEntireLogMsg(int pid) {
+  std::stringstream buf;
+  buf << msg_time_    << " ";
+  buf << msg_level_   << " ";
+  buf << pid          << " ";
+  buf << msg_module_  << " ";
+  buf << msg_file_    << ":";
+  buf << msg_line_    << " ";
+  buf << msg_data_;
+  return buf.str();
 }
 
-Log::Log(){
-    current_length_ = 0;
-    char * log_path = getenv("MSG_LOG_PATH");
-    if (log_path == NULL){
-        log_path = getcwd(NULL, 0);
-        log_path_ = log_path;
-        free(log_path);
-        return;
-    }
+Log::Log() {
+  char * log_path = getenv("MSG_LOG_PATH");
+  if (log_path == NULL) {
+    log_path = getcwd(NULL, 0);
     log_path_ = log_path;
+    free(log_path);
+    return;
+  }
+  log_path_ = log_path;
 }
 
-//multithread safe
-void Log::log(LogMsg* msg){
-    std::lock_guard<std::mutex> lg(log_lock_);
-    log_buffer_[current_length_++] = msg;
-        if(current_length_ == max_length_){
-        write_down();
-    }
+// multithread safe
+void Log::LogWrite(const LogMsg& msg) {
+  std::lock_guard<std::mutex> lg(log_lock_);
+  log_queue_.push(msg);
+    if (log_queue_.size() == MAX_SIZE_LOG) {
+    Write_down();
+  }
 }
 
-//multithread unsafe
-void Log::clear(){
-    int i = 0;
-    for(;i<current_length_;i++){
-        delete log_buffer_[i];
-    }
-    current_length_ = 0;
+// multithread unsafe
+void Log::Clear() {
+  std::queue<LogMsg>().swap(log_queue_);
 }
 
-//multithread unsafe
-void Log::write_down(){
-    int i = 0;
-    signed int pid = getPid();
-    std::stringstream log_file_name;
-    log_file_name << log_path_ << "/" <<DEBUG_LOG_MODULE << "_pid_" << pid << "." << DEBUG_LOG_FILE_SUFFIX;
-    std::fstream log_file;
-    log_file.open(log_file_name.str(), std::ios::app);
-    for(;i<current_length_;i++){
-        std::string log_str = (*log_buffer_[i]).getEntireLogMsg(pid);
-        log_file << log_str;
-    }
-    log_file.close();
-    clear();
+// multithread unsafe
+void Log::Write_down() {
+  int i = 0;
+  signed int pid = getPid();
+  std::stringstream log_file_name;
+  log_file_name << log_path_ << "/" << DEBUG_LOG_MODULE << "_pid_" << pid << "." << DEBUG_LOG_FILE_SUFFIX;
+  std::fstream log_file;
+  log_file.open(log_file_name.str(), std::ios::app);
+  while (!log_queue_.empty()) {
+    std::string log_str = log_queue_.front().getEntireLogMsg(pid);
+    log_file << log_str;
+    log_queue_.pop();
+  }
+  log_file.close();
 }
 
-Log::~Log(){
-    write_down();
-    // clear();
+Log::~Log() {
+  Write_down();
 }
 
-signed int getPid(){
-return getpid();
+signed int getPid() {
+  return getpid();
 }
 
-std::string getTime(){
+std::string getTime() {
   char buf[MAX_SIZE_LOG_TIME] = {};
   struct timespec ts;
   timespec_get(&ts, TIME_UTC);
   int end = strftime(buf, sizeof(buf), "%Y-%m-%d.%X", gmtime(&ts.tv_sec));
-  snprintf(buf + end, MAX_SIZE_LOG_TIME-end," %09ld", ts.tv_nsec);
+  snprintf(buf + end, MAX_SIZE_LOG_TIME-end, " %09ld", ts.tv_nsec);
   std::string ret = std::string(buf);
   return ret;
 }
 
-void LogClear(){
+void LogClear() {
 #if DEBUG_MODE && DEBUG_LOG_TO_FILE
-    getLogInstance()->clear();
+  getLogInstance()->Clear();
 #endif
 }
 
 LogPtr log_instance;
 
-LogPtr getLogInstance(){
+LogPtr getLogInstance() {
   if (log_instance == nullptr)
     log_instance = std::make_shared<Log>();
   return log_instance;
 }
 
-} // namespace message_infrastructure
+}  // namespace message_infrastructure
--- src/lava/magma/runtime/message_infrastructure/message_infrastructure/csrc/core/message_infrastructure_logging.h
+++ src/lava/magma/runtime/message_infrastructure/message_infrastructure/csrc/core/message_infrastructure_logging.h
@@ -7,92 +7,117 @@
 
 #include <memory>
 #include <mutex>
-#include <thread>
-#include <iostream> 
-#include <sstream> 
+#include <iostream>
+#include <sstream>
 #include <fstream>
 #include <string>
 #include <cstdlib>
-
-#include <stdio.h>
-#include <memory.h>
-#include <time.h>
+#include <ctime>
+#include <queue>
 
 #if _WIN32
 #inlcude <process.h>
-#include <direct.h> // _getcwd()
+#include <direct.h>  // _getcwd()
 #define getpid() _getpid()
 #define getcwd() _getcwd()
-#else //__linux__ & __APPLE__
+#else  // __linux__ & __APPLE__
 #include <unistd.h>
 #include <sys/types.h>
 #endif
 
-#ifdef CMAKE_DEBUG
-#define DEBUG_MODE (1)
-#else
-#define DEBUG_MODE (0)
-#endif
-
+#define MAX_SIZE_PER_LOG_MSG (1024)
+#define MAX_SIZE_LOG (1)
+#define MAX_SIZE_LOG_TIME (64)
+#define LOG_MSG_SUBSTITUTION "!This message was displayed due to the failure of the malloc of this log message!\n"
+#define LOG_GET_TIME_FAIL "Get log time failed."
 #define DEBUG_LOG_MODULE "lava_message_infrastructure"
 #define DEBUG_LOG_FILE_SUFFIX "log"
 #define NULL_STRING ""
 
-//#if DEBUG_MODE
-#define DEBUG_LOG_TO_FILE (1)
+#define LOG_MASK_NULL (0)
+#define LOG_MASK_INFO (1)
+#define LOG_MASK_DUMP (1<<1)
+#define LOG_MASK_DBUG (1<<2)
+#define LOG_MASK_WARN (1<<3)
+#define LOG_MASK_ERRO (1<<4)
 
-#define MAX_SIZE_PER_LOG_MSG (1024)
-#define MAX_SIZE_LOG (1)
-#define MAX_SIZE_LOG_TIME (64)
+#define LOG_PRINT_MASK_NULL (0)
+#define LOG_PRINT_MASK_SHEL (1)
+#define LOG_PRINT_MASK_FILE (2)
 
-#define LOG_MSG_SUBSTITUTION "!This message was displayed due to the failure of the malloc of this log message!\n"
-#define LOG_GET_TIME_FAIL "Get log time failed."
+#if defined(MSG_LOG_LEVEL)
 
-#if DEBUG_MODE && DEBUG_LOG_TO_FILE
-#define DEBUG_LOG_PRINT(_level,_module, _fmt, ...) do {\
-  int length = 0;\
-  char * log_data = (char*)malloc(sizeof(char)*MAX_SIZE_PER_LOG_MSG);\
-  if (log_data != NULL)\
-    length = std::snprintf(log_data, MAX_SIZE_PER_LOG_MSG, _fmt,  ## __VA_ARGS__);\
-  if (log_data == NULL || length <0){\
-    getLogInstance()->log(new LogMsg(std::string(LOG_MSG_SUBSTITUTION), __FILE__, __LINE__, _module, _level));\
-  }else{\
-    getLogInstance()->log(new LogMsg(std::string(log_data), __FILE__, __LINE__, _module, _level));\
-  }\
-  free(log_data);\
-}while(0)
-
+#elif defined(MSG_LOG_LEVEL_ALL)
+  #define MSG_LOG_LEVEL (LOG_MASK_INFO | LOG_MASK_ERRO | LOG_MASK_DUMP | LOG_MASK_WARN | LOG_MASK_DBUG)
+#elif defined(MSG_LOG_LEVEL_WARN_ERR)
+  #define MSG_LOG_LEVEL (LOG_MASK_ERRO | LOG_MASK_WARN)
 #else
-#define DEBUG_LOG_PRINT(_level,_module, ...) //std::printf(__VA_ARGS__)
+  #define MSG_LOG_LEVEL (LOG_MASK_ERRO)  // default
 #endif
 
-#define LAVA_LOG(_module, _fmt, ...) { \
-  if ((_module)) { \
-    DEBUG_LOG_PRINT("[CPP INFO]", _module, _fmt, ## __VA_ARGS__);\
-  } \
-}
+#if defined(MSG_LOG_PRINT_MODE)
 
-#define LAVA_DUMP(_module, _fmt, ...) { \
-  if ((_module && DEBUG_MODE)) { \
-    DEBUG_LOG_PRINT("[CPP DUMP]", _module, _fmt, ## __VA_ARGS__);\
-  } \
-}
+#elif defined(MSG_LOG_PRINT_MODE_ALL)
+  #define MSG_LOG_PRINT_MODE (LOG_PRINT_MASK_SHEL | LOG_PRINT_MASK_FILE)
+#elif defined(MSG_LOG_PRINT_MODE_FILE)
+  #define MSG_LOG_PRINT_MODE (LOG_PRINT_MASK_FILE)
+#elif defined(MSG_LOG_PRINT_MODE_NULL)
+  #define MSG_LOG_PRINT_MODE (LOG_PRINT_MASK_NULL)
+#else
+  #define MSG_LOG_PRINT_MODE (LOG_PRINT_MASK_SHEL)  // default
+#endif
 
-#define LAVA_DEBUG(_module, _fmt, ...) { \
-  if ((_module && DEBUG_MODE)) { \
-    DEBUG_LOG_PRINT("[CPP DBUG]", _module, _fmt, ## __VA_ARGS__);\
+#if (MSG_LOG_PRINT_MODE)
+#define DEBUG_LOG_PRINT(_level, _module, _fmt, ...) do { \
+  if (MSG_LOG_PRINT_MODE & LOG_PRINT_MASK_FILE) { \
+    int length = 0; \
+    char * log_data = (char*)malloc(sizeof(char)*MAX_SIZE_PER_LOG_MSG); \
+    if (log_data != NULL) \
+      length = std::snprintf(log_data, MAX_SIZE_PER_LOG_MSG, _fmt,  ## __VA_ARGS__); \
+    if (log_data == NULL || length <0) { \
+      getLogInstance()->LogWrite(LogMsg(std::string(LOG_MSG_SUBSTITUTION), __FILE__, __LINE__, _module, _level)); \
+    } else { \
+      getLogInstance()->LogWrite(LogMsg(std::string(log_data), __FILE__, __LINE__, _module, _level)); \
+    } \
+    free(log_data); \
   } \
-}
-
-#define LAVA_LOG_WARN(_module, _fmt, ...) { \
-  if ((_module)) { \
-    DEBUG_LOG_PRINT("[CPP WARN]", _module, _fmt, ## __VA_ARGS__);\
+  if (MSG_LOG_PRINT_MODE & LOG_PRINT_MASK_SHEL) { \
+    std::printf(_fmt, ## __VA_ARGS__); \
   } \
-}
+}while(0)
+#else
+#define DEBUG_LOG_PRINT(_level, _module, ...)
+#endif
 
-#define LAVA_LOG_ERR(_module, _fmt, ...) { \
-  DEBUG_LOG_PRINT("[CPP ERRO]", _module, _fmt, ## __VA_ARGS__);\
-}
+#define LAVA_LOG(_module, _fmt, ...) do { \
+  if ((_module) && (MSG_LOG_LEVEL & LOG_MASK_INFO)) { \
+    DEBUG_LOG_PRINT("[CPP INFO]", _module, _fmt, ## __VA_ARGS__); \
+  } \
+}while(0)
+
+#define LAVA_DUMP(_module, _fmt, ...) do { \
+  if (_module && (MSG_LOG_LEVEL & LOG_MASK_DUMP)) { \
+    DEBUG_LOG_PRINT("[CPP DUMP]", _module, _fmt, ## __VA_ARGS__); \
+  } \
+}while(0)
+
+#define LAVA_DEBUG(_module, _fmt, ...) do { \
+  if (_module && (MSG_LOG_LEVEL & LOG_MASK_DBUG)) { \
+    DEBUG_LOG_PRINT("[CPP DBUG]", _module, _fmt, ## __VA_ARGS__); \
+  } \
+}while(0)
+
+#define LAVA_LOG_WARN(_module, _fmt, ...) do { \
+  if (_module && (MSG_LOG_LEVEL & LOG_MASK_WARN)) { \
+    DEBUG_LOG_PRINT("[CPP WARN]", _module, _fmt, ## __VA_ARGS__); \
+  } \
+}while(0)
+
+#define LAVA_LOG_ERR(_module, _fmt, ...) do { \
+  if (_module && (MSG_LOG_LEVEL & LOG_MASK_ERRO)) { \
+    DEBUG_LOG_PRINT("[CPP ERRO]", _module, _fmt, ## __VA_ARGS__); \
+  } \
+}while(0)
 
 namespace message_infrastructure {
 
@@ -114,36 +139,35 @@ enum Log_Module{
 };
 
 class LogMsg{
-  public:
-    LogMsg(const std::string&, const char* log_file, int log_line, enum Log_Module log_module, const char* log_level);
-    std::string getEntireLogMsg(int pid);
+ public:
+  LogMsg(const std::string&,
+         const char* log_file,
+         int log_line,
+         enum Log_Module log_module,
+         const char* log_level);
+  std::string getEntireLogMsg(int pid);
 
-  private:
-  // std::string msg_time_ = nullptr; this way will cause a abort trouble...
-    std::string msg_time_;
-    std::string msg_data_;
-    std::string msg_level_;
-    std::string msg_module_;
-    std::string msg_file_ ;
-    std::string msg_pid_;
-    int msg_line_ = 0;
+ private:
+  std::string msg_time_;
+  std::string msg_data_;
+  std::string msg_level_;
+  std::string msg_module_;
+  std::string msg_file_;
+  int msg_line_ = 0;
 };
 
 class Log{
-  public:
-    Log();
-    ~Log();
-    void log(LogMsg*);
-    void clear();
-    void write_down();
-    
+ public:
+  Log();
+  ~Log();
+  void LogWrite(const LogMsg& msg);
+  void Clear();
+  void Write_down();
 
-  private:
-    std::string log_path_;
-    int current_length_ = 0;
-    const int max_length_ = MAX_SIZE_LOG;
-    std::mutex log_lock_;
-    LogMsg * log_buffer_[MAX_SIZE_LOG];
+ private:
+  std::string log_path_;
+  std::mutex log_lock_;
+  std::queue<LogMsg> log_queue_;
 };
 
 using LogPtr = std::shared_ptr<Log>;
@@ -156,6 +180,6 @@ signed int getPid();
 
 void LogClear();
 
-} // namespace message_infrastructure
+}  // namespace message_infrastructure
 
 #endif  // MESSAGE_INFRASTRUCTURE_LOGGING_H_
