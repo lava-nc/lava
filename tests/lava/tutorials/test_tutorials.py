bcf5c433f310351cb058cebf82b829fbbfd8f9a4
bala <balachandran.swaminathan@intel.com>
2022-10-25 11:59:27 -0700

    changing tutorial name

--- tests/lava/tutorials/test_tutorials.py
+++ tests/lava/tutorials/test_tutorials.py
@@ -272,7 +272,7 @@ class TestTutorials(unittest.TestCase):
     @unittest.skipIf(system_name != "linux", "Tests work on linux")
     def test_in_depth_10_three_factor_learning_rules(self):
         """Test tutorial three factor learning rules."""
-        self._run_notebook("tutorial10_three_factor_learning.ipynb")
+        self._run_notebook("tutorial01_Reward_Modulated_STDP.ipynb")
 
 
 if __name__ == "__main__":
