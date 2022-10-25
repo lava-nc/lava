47ab2b82d96d3f10e13584c832c8488431fec102
PhilippPlank <32519998+PhilippPlank@users.noreply.github.com>
2022-10-25 19:27:45 +0200

    Update profiler.py

--- src/lava/utils/profiler.py
+++ src/lava/utils/profiler.py
@@ -2,13 +2,30 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # See: https://spdx.org/licenses/
 
-"""
-This module will contain a tool to determine power and performance of workloads
-for Loihi 1 or Loihi 2 based on software simulations or hardware measurements.
+from lava.magma.core.run_configs import RunConfig, Loihi2HwCfg
 
-The execution time and energy of a workload will be either measured on hardware
-during execution or estimated in simulation. The estimation is based on
-elementary hardware operations which are counted during the simulation. Each
-elementary operation has a defined execution time and energy cost, which is
-used in a performance model to calculate execution time and energy.
-"""
+
+class Profiler:
+    """Base class for profiling execution time, energy and other
+    metrics on different Ressources. Depending on the computing
+    ressource an appropriate profiler needs to be chosen. The run
+    configuration is used to choose the related profiler, if there
+    is one."""
+
+    run_cfg_dict = {Loihi2HwCfg: Loihi2HWProfiler}
+
+    @staticmethod
+    def init(run_cfg: RunConfig):
+        """Decide which profiler is needed based on the run
+        configuration."""
+
+        if not isinstance(run_cfg, RunConfig):
+            raise AssertionError("<run_cfg> must be an "
+                                 "instance of {}".format(RunConfig))
+
+        if not type(run_cfg) in Profiler.run_cfg_dict:
+            raise NotImplementedError(
+                f"There is currently no implementation of the "
+                f"profiler for {type(run_cfg).__name__}.")
+
+        return Profiler.run_cfg_dict[type(run_cfg)](run_cfg)
