14d7d9025662a8d9338bcf7348ffe9ebb0f9c18e
luoxiaoc <xiaochuan.luo@intel.com>
2022-10-27 21:50:33 +0800

    enable parallel build
    Signed-off-by: luoxiaoc <xiaochuan.luo@intel.com>

--- src/lava/magma/runtime/message_infrastructure/setup.py
+++ src/lava/magma/runtime/message_infrastructure/setup.py
@@ -1,6 +1,7 @@
 import os
 import re
 import subprocess  # nosec
+import multiprocessing
 import sys
 import numpy
 
@@ -115,12 +116,8 @@ class CMakeBuild(build_ext):
         # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
         # across all generators.
         if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
-            # self.parallel is a Python 3 only way to set parallel jobs by hand
-            # using -j in the build_ext call,
-            #   not supported by pip or PyPA-build.
-            if hasattr(self, "parallel") and self.parallel:
-                # CMake 3.12+ only.
-                build_args += [f"-j{self.parallel}"]
+            self.parallel = multiprocessing.cpu_count() if not self.parallel else self.parallel
+            build_args += [f"-j{self.parallel}"]
 
         build_temp = os.path.join(self.build_temp, ext.name)
         if not os.path.exists(build_temp):
