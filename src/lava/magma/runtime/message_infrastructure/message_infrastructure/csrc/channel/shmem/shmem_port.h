73058583a988976a5d510f8c09b15d40194e7f5a
szc321 <zongcai.shi@intel.com>
2022-10-31 10:51:09 +0800

    use common.h in shmem_port

--- src/lava/magma/runtime/message_infrastructure/message_infrastructure/csrc/channel/shmem/shmem_port.h
+++ src/lava/magma/runtime/message_infrastructure/message_infrastructure/csrc/channel/shmem/shmem_port.h
@@ -1,4 +1,3 @@
-
 // Copyright (C) 2022 Intel Corporation
 // SPDX-License-Identifier: BSD-3-Clause
 // See: https://spdx.org/licenses/
@@ -8,6 +7,7 @@
 
 #include <message_infrastructure/csrc/core/abstract_port.h>
 #include <message_infrastructure/csrc/channel/shmem/shm.h>
+#include <message_infrastructure/csrc/core/common.h>
 
 #include <string>
 #include <vector>
@@ -39,39 +39,13 @@ class ShmemSendPort final : public AbstractSendPort {
 
 using ShmemSendPortPtr = std::shared_ptr<ShmemSendPort>;
 
-class ShmemRecvQueue {
- public:
-  ShmemRecvQueue(const std::string& name,
-                 const size_t &size,
-                 const size_t &nbytes);
-  ~ShmemRecvQueue();
-  void Push(void* src);
-  void* Pop(bool block);
-  void* Front();
-  bool Probe();
-  bool Empty();
-  int AvailableCount();
-  void Free();
-  void Stop();
-
- private:
-  std::string name_;
-  size_t size_;
-  size_t nbytes_;
-  std::vector<void *> array_;
-  std::atomic<uint32_t> read_index_;
-  std::atomic<uint32_t> write_index_;
-  std::atomic_bool done_;
-};
-
-using ShmemRecvQueuePtr = std::shared_ptr<ShmemRecvQueue>;
-
 class ShmemRecvPort final : public AbstractRecvPort {
  public:
   ShmemRecvPort(const std::string &name,
                 SharedMemoryPtr shm,
                 const size_t &size,
                 const size_t &nbytes);
+  ~ShmemRecvPort();
   void Start();
   bool Probe();
   MetaDataPtr Recv();
@@ -83,7 +57,7 @@ class ShmemRecvPort final : public AbstractRecvPort {
   SharedMemoryPtr shm_ = nullptr;
   int idx_ = 0;
   std::atomic_bool done_;
-  ShmemRecvQueuePtr queue_ = nullptr;
+  std::shared_ptr<RecvQueue<void*>> recvqueue;
   ThreadPtr recv_queue_thread_ = nullptr;
 };
 
@@ -92,4 +66,3 @@ using ShmemRecvPortPtr = std::shared_ptr<ShmemRecvPort>;
 }  // namespace message_infrastructure
 
 #endif  // CHANNEL_SHMEM_SHMEM_PORT_H_
-
