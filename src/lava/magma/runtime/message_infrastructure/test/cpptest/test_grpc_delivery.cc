bc27b228b7beb385a7641338e3702ce207c2fb7d
szc321 <zongcai.shi@intel.com>
2022-11-08 14:35:48 +0800

    fix conflicts and add socket cpput
    Signed-off-by: szc321 <zongcai.shi@intel.com>

--- src/lava/magma/runtime/message_infrastructure/test/cpptest/test_grpc_delivery.cc
+++ src/lava/magma/runtime/message_infrastructure/test/cpptest/test_grpc_delivery.cc
@@ -16,7 +16,7 @@ static void stop_fn() {
   // exit(0);
 }
 
-void target_fn1(
+void grpc_target_fn1(
   int loop,
   AbstractChannelPtr mp_to_a1,
   AbstractChannelPtr a1_to_mp,
@@ -34,9 +34,9 @@ void target_fn1(
     from_a2->Start();
     LAVA_DUMP(1, "grpc actor1, loop: %d\n", loop);
     while ((loop--)&&!actor_ptr->GetStatus()) {
-      LAVA_DUMP(1, "grpc actor1 waitting\n");
+      // LAVA_DUMP(1, "grpc actor1 waitting\n");
       MetaDataPtr data = from_mp->Recv();
-      LAVA_DUMP(1, "grpc actor1 recviced\n");
+      // LAVA_DUMP(1, "grpc actor1 recviced\n");
       (*reinterpret_cast<int64_t*>(data->mdata))++;
       to_a2->Send(data);
       free(reinterpret_cast<char*>(data->mdata));
@@ -54,7 +54,7 @@ void target_fn1(
     }
   }
 
-void target_fn2(
+void grpc_target_fn2(
   int loop,
   AbstractChannelPtr a1_to_a2,
   AbstractChannelPtr a2_to_a1,
@@ -64,11 +64,11 @@ void target_fn2(
     auto from_a1 = a1_to_a2->GetRecvPort();
     from_a1->Start();
     to_a1->Start();
-    LAVA_DUMP(1, "grpc actor2, loop: %d\n", loop);
+    // LAVA_DUMP(1, "grpc actor2, loop: %d\n", loop);
     while ((loop--)&&!actor_ptr->GetStatus()) {
-      LAVA_DUMP(1, "grpc actor2 waitting\n");
+      // LAVA_DUMP(1, "grpc actor2 waitting\n");
       MetaDataPtr data = from_a1->Recv();
-      LAVA_DUMP(1, "grpc actor2 recviced\n");
+      // LAVA_DUMP(1, "grpc actor2 recviced\n");
       (*reinterpret_cast<int64_t*>(data->mdata))++;
       to_a1->Send(data);
       free(reinterpret_cast<char*>(data->mdata));
@@ -91,14 +91,14 @@ TEST(TestGRPCChannel, GRPCLoop) {
     "a1_to_a2", "a1_to_a2", 6);
   AbstractChannelPtr a2_to_a1 = GetChannelFactory().GetDefRPCChannel(
     "a2_to_a1", "a2_to_a1", 6);
-  auto target_fn_a1 = std::bind(&target_fn1,
+  auto target_fn_a1 = std::bind(&grpc_target_fn1,
                                 loop,
                                 mp_to_a1,
                                 a1_to_mp,
                                 a1_to_a2,
                                 a2_to_a1,
                                 std::placeholders::_1);
-  auto target_fn_a2 = std::bind(&target_fn2,
+  auto target_fn_a2 = std::bind(&grpc_target_fn2,
                                 loop,
                                 a1_to_a2,
                                 a2_to_a1,
@@ -109,17 +109,19 @@ TEST(TestGRPCChannel, GRPCLoop) {
   to_a1->Start();
   auto from_a1 = a1_to_mp->GetRecvPort();
   from_a1->Start();
-  int64_t array_[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+  int64_t array_[10000] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+  std::fill(array_ + 10, array_ + 10000, 1);
+
   MetaDataPtr metadata = std::make_shared<MetaData>();
   metadata->nd = 1;
   metadata->type = 7;
   metadata->elsize = 8;
-  metadata->total_size = 10;
-  metadata->dims[0] = 10;
+  metadata->total_size = 10000;
+  metadata->dims[0] = 10000;
   metadata->strides[0] = 1;
   metadata->mdata =
     reinterpret_cast<char*>
-    (malloc(sizeof(int64_t)*10));
+    (malloc(sizeof(int64_t)*10000));
   std::memcpy(metadata->mdata,
               reinterpret_cast<char*>(array_),
               metadata->elsize * metadata->total_size);
@@ -128,25 +130,26 @@ TEST(TestGRPCChannel, GRPCLoop) {
   const clock_t start_time = std::clock();
   while (loop--) {
     to_a1->Send(metadata);
-    LAVA_DUMP(1, "wait for response, remain loop: %d\n", loop);
+    // LAVA_DUMP(1, "wait for response, remain loop: %d\n", loop);
     mptr = from_a1->Recv();
-    LAVA_DUMP(1, "metadata:\n");
-    LAVA_DUMP(1, "nd: %ld\n", mptr->nd);
-    LAVA_DUMP(1, "type: %ld\n", mptr->type);
-    LAVA_DUMP(1, "elsize: %ld\n", mptr->elsize);
-    LAVA_DUMP(1, "total_size: %ld\n", mptr->total_size);
-    LAVA_DUMP(1, "dims: {%ld, %ld, %ld, %ld, %ld}\n",
-    mptr->dims[0], mptr->dims[1], mptr->dims[2], mptr->dims[3], mptr->dims[4]);
-    LAVA_DUMP(1, "strides: {%ld, %ld, %ld, %ld, %ld}\n",
-    mptr->strides[0], mptr->strides[1], mptr->strides[2], mptr->strides[3],
-    mptr->strides[4]);
+    // LAVA_DUMP(1, "metadata:\n");
+    // LAVA_DUMP(1, "nd: %ld\n", mptr->nd);
+    // LAVA_DUMP(1, "type: %ld\n", mptr->type);
+    // LAVA_DUMP(1, "elsize: %ld\n", mptr->elsize);
+    // LAVA_DUMP(1, "total_size: %ld\n", mptr->total_size);
+    // LAVA_DUMP(1, "dims: {%ld, %ld, %ld, %ld, %ld}\n",
+              // mptr->dims[0], mptr->dims[1], mptr->dims[2],
+              // mptr->dims[3], mptr->dims[4]);
+    // LAVA_DUMP(1, "strides: {%ld, %ld, %ld, %ld, %ld}\n",
+    // mptr->strides[0], mptr->strides[1], mptr->strides[2], mptr->strides[3],
+    // mptr->strides[4]);
     int64_t *ptr = reinterpret_cast<int64_t*>(mptr->mdata);
-    for (int i = 0; i < 10; i++) {
-      LAVA_DUMP(1, "grpc mdata: %p, grpc *mdata: %ld\n", ptr, *ptr);
-      ptr++;
-    }
-    LAVA_DUMP(1, "mdata: %p, *mdata: %ld\n", mptr->mdata,
-              *reinterpret_cast<int64_t*>(mptr->mdata));
+    // for (int i = 0; i < 20; i++) {
+    //   LAVA_DUMP(1, "grpc mdata: %p, grpc *mdata: %ld\n", ptr, *ptr);
+    //   ptr++;
+    // }
+    // LAVA_DUMP(1, "grpc mdata: %p, grpc *mdata: %ld\n", mptr->mdata,
+    //           *reinterpret_cast<int64_t*>(mptr->mdata));
     free(reinterpret_cast<char*>(metadata->mdata));
     metadata = mptr;
   }
@@ -154,6 +157,7 @@ TEST(TestGRPCChannel, GRPCLoop) {
   to_a1->Join();
   from_a1->Join();
   int64_t result = *reinterpret_cast<int64_t*>(metadata->mdata);
+  printf("grpc result =%ld", result);
   free(reinterpret_cast<char*>(metadata->mdata));
   mp.Stop(true);
   if (result != expect_result) {
--- src/lava/magma/runtime/message_infrastructure/test/cpptest/test_shm_delivery.cc
+++ src/lava/magma/runtime/message_infrastructure/test/cpptest/test_shm_delivery.cc
@@ -31,11 +31,11 @@ void target_fn_a1_bound(
     to_a2->Start();
     auto from_a2 = a2_to_a1->GetRecvPort();
     from_a2->Start();
-    LAVA_DUMP(1, "actor1, loop: %d\n", loop);
+    // LAVA_DUMP(1, "shm actor1, loop: %d\n", loop);
     while ((loop--)&&!actor_ptr->GetStatus()) {
-      LAVA_DUMP(1, "actor1 waitting\n");
+      // LAVA_DUMP(1, "shm actor1 waitting\n");
       MetaDataPtr data = from_mp->Recv();
-      LAVA_DUMP(1, "actor1 recviced\n");
+      // LAVA_DUMP(1, "shm actor1 recviced\n");
       (*reinterpret_cast<int64_t*>(data->mdata))++;
       to_a2->Send(data);
       free(reinterpret_cast<char*>(data->mdata));
@@ -61,11 +61,11 @@ void target_fn_a2_bound(
     from_a1->Start();
     auto to_a1   = a2_to_a1->GetSendPort();
     to_a1->Start();
-    LAVA_DUMP(1, "actor2, loop: %d\n", loop);
+    // LAVA_DUMP(1, "shm actor2, loop: %d\n", loop);
     while ((loop--)&&!actor_ptr->GetStatus()) {
-      LAVA_DUMP(1, "actor2 waitting\n");
+      // LAVA_DUMP(1, "shm actor2 waitting\n");
       MetaDataPtr data = from_a1->Recv();
-      LAVA_DUMP(1, "actor2 recviced\n");
+      // LAVA_DUMP(1, "shm actor2 recviced\n");
       (*reinterpret_cast<int64_t*>(data->mdata))++;
       to_a1->Send(data);
       free(reinterpret_cast<char*>(data->mdata));
@@ -81,13 +81,13 @@ TEST(TestShmDelivery, ShmLoop) {
   int loop = 10000;
   const int queue_size = 1;
   AbstractChannelPtr mp_to_a1 = GetChannelFactory().GetChannel(
-    SHMEMCHANNEL, queue_size, sizeof(int64_t)*10, "mp_to_a1", "mp_to_a1");
+    SHMEMCHANNEL, queue_size, sizeof(int64_t)*10000, "mp_to_a1", "mp_to_a1");
   AbstractChannelPtr a1_to_mp = GetChannelFactory().GetChannel(
-    SHMEMCHANNEL, queue_size, sizeof(int64_t)*10, "a1_to_mp", "a1_to_mp");
+    SHMEMCHANNEL, queue_size, sizeof(int64_t)*10000, "a1_to_mp", "a1_to_mp");
   AbstractChannelPtr a1_to_a2 = GetChannelFactory().GetChannel(
-    SHMEMCHANNEL, queue_size, sizeof(int64_t)*10, "a1_to_a2", "a1_to_a2");
+    SHMEMCHANNEL, queue_size, sizeof(int64_t)*10000, "a1_to_a2", "a1_to_a2");
   AbstractChannelPtr a2_to_a1 = GetChannelFactory().GetChannel(
-    SHMEMCHANNEL, queue_size, sizeof(int64_t)*10, "a2_to_a1", "a2_to_a1");
+    SHMEMCHANNEL, queue_size, sizeof(int64_t)*10000, "a2_to_a1", "a2_to_a1");
   auto target_fn_a1 = std::bind(&target_fn_a1_bound, loop,
                                 mp_to_a1, a1_to_mp, a1_to_a2,
                                 a2_to_a1, std::placeholders::_1);
@@ -99,17 +99,19 @@ TEST(TestShmDelivery, ShmLoop) {
   to_a1->Start();
   auto from_a1 = a1_to_mp->GetRecvPort();
   from_a1->Start();
-  int64_t array_[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+  int64_t array_[10000] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+  std::fill(array_ + 10, array_ + 10000, 1);
+
   MetaDataPtr metadata = std::make_shared<MetaData>();
   metadata->nd = 1;
   metadata->type = 7;
   metadata->elsize = 8;
-  metadata->total_size = 10;
-  metadata->dims[0] = 10;
+  metadata->total_size = 10000;
+  metadata->dims[0] = 10000;
   metadata->strides[0] = 1;
   metadata->mdata =
     reinterpret_cast<char*>
-    (malloc(sizeof(int64_t)*10));
+    (malloc(sizeof(int64_t)*10000));
   std::memcpy(metadata->mdata,
               reinterpret_cast<char*>(array_),
               metadata->elsize * metadata->total_size);
@@ -120,30 +122,32 @@ TEST(TestShmDelivery, ShmLoop) {
   while (loop--) {
     to_a1->Send(metadata);
     free(reinterpret_cast<char*>(metadata->mdata));
-    LAVA_DUMP(1, "wait for response, remain loop: %d\n", loop);
+    // LAVA_DUMP(1, "shm wait for response, remain loop: %d\n", loop);
     mptr = from_a1->Recv();
-    to_a1->Join();
-    LAVA_DUMP(1, "metadata:\n");
-    LAVA_DUMP(1, "nd: %ld\n", mptr->nd);
-    LAVA_DUMP(1, "type: %ld\n", mptr->type);
-    LAVA_DUMP(1, "elsize: %ld\n", mptr->elsize);
-    LAVA_DUMP(1, "total_size: %ld\n", mptr->total_size);
-    LAVA_DUMP(1, "dims: {%ld, %ld, %ld, %ld, %ld}\n",
-    mptr->dims[0], mptr->dims[1], mptr->dims[2], mptr->dims[3], mptr->dims[4]);
-    LAVA_DUMP(1, "strides: {%ld, %ld, %ld, %ld, %ld}\n",
-    mptr->strides[0], mptr->strides[1], mptr->strides[2], mptr->strides[3],
-    mptr->strides[4]);
-    LAVA_DUMP(1, "mdata: %p, *mdata: %ld\n", mptr->mdata,
-              *reinterpret_cast<int64_t*>(mptr->mdata));
+    // to_a1->Join();
+    // LAVA_DUMP(1, "metadata:\n");
+    // LAVA_DUMP(1, "nd: %ld\n", mptr->nd);
+    // LAVA_DUMP(1, "type: %ld\n", mptr->type);
+    // LAVA_DUMP(1, "elsize: %ld\n", mptr->elsize);
+    // LAVA_DUMP(1, "total_size: %ld\n", mptr->total_size);
+    // LAVA_DUMP(1, "dims: {%ld, %ld, %ld, %ld, %ld}\n",
+              // mptr->dims[0], mptr->dims[1], mptr->dims[2],
+              // mptr->dims[3], mptr->dims[4]);
+    // LAVA_DUMP(1, "strides: {%ld, %ld, %ld, %ld, %ld}\n",
+    // mptr->strides[0], mptr->strides[1], mptr->strides[2], mptr->strides[3],
+    // mptr->strides[4]);
+    // LAVA_DUMP(1, "mdata: %p, *mdata: %ld\n", mptr->mdata,
+    //           *reinterpret_cast<int64_t*>(mptr->mdata));
     int64_t *ptr = reinterpret_cast<int64_t*>(mptr->mdata);
-    for (int i = 0; i < 10; i++) {
-      LAVA_DUMP(1, "shm mdata: %p, shm *mdata: %ld\n", ptr, *ptr);
-      ptr++;
-    }
+    // for (int i = 0; i < 20; i++) {
+    //   LAVA_DUMP(1, "shm mdata: %p, shm *mdata: %ld\n", ptr, *ptr);
+    //   ptr++;
+    // }
     metadata = mptr;
   }
   const clock_t end_time = std::clock();
   int64_t result = *reinterpret_cast<int64_t*>(metadata->mdata);
+  printf("shm result =%ld", result);
   free(reinterpret_cast<char*>(mptr->mdata));
   from_a1->Join();
   mp.Stop(true);
