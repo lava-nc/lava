3c99ef8a1fffbeff4ec8ea84aa145feee36b7e17
Xu, He <he.xu@intel.com>
2022-11-25 05:31:00 +0000

    Fix lint errors
    Signed-off-by: Xu, He <he.xu@intel.com>

--- src/lava/magma/runtime/message_infrastructure/examples/ros2/ros_talk_with_dds_py/ros_talk_with_dds_py/publisher.py
+++ src/lava/magma/runtime/message_infrastructure/examples/ros2/ros_talk_with_dds_py/ros_talk_with_dds_py/publisher.py
@@ -9,19 +9,20 @@ from rclpy.qos import qos_profile_system_default
 from ddsmetadata.msg import DDSMetaData
 from .utils.np_mdata_trans import nparray_to_metadata
 
+
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        ros_qos = qos_profile_system_default
-        self.publisher_ = self.create_publisher(DDSMetaData, 'dds_topic', ros_qos)
+        self.publisher_ = self.create_publisher(DDSMetaData,
+                                                'dds_topic',
+                                                qos_profile_system_default)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
-        
 
     def timer_callback(self):
-        np_arr = np.array(([self.i,2,3]), np.int64)
+        np_arr = np.array(([self.i, 2, 3]), np.int64)
         msg = nparray_to_metadata(np_arr)
         self.publisher_.publish(msg)
         print("Publishing : ", np_arr)
--- src/lava/magma/runtime/message_infrastructure/examples/ros2/ros_talk_with_dds_py/ros_talk_with_dds_py/utils/np_mdata_trans.py
+++ src/lava/magma/runtime/message_infrastructure/examples/ros2/ros_talk_with_dds_py/ros_talk_with_dds_py/utils/np_mdata_trans.py
@@ -4,17 +4,17 @@ from ddsmetadata.msg import DDSMetaData
 
 
 def dtype_num_to_char(num):
-  trans = ['?','b','B','h','H','l','L',
-           'l','L','q','Q','d','d','g',
-           'D','D','G','O','S','U','V']
-  return trans[num]
+    trans = ['?', 'b', 'B', 'h', 'H', 'l', 'L',
+             'l', 'L', 'q', 'Q', 'd', 'd', 'g',
+             'D', 'D', 'G', 'O', 'S', 'U', 'V']
+    return trans[num]
 
 
 def metadata_to_nparray(metadata):
     ndim = metadata.nd
     shape = metadata.dims
     dims = list()
-    for i in range (ndim):
+    for i in range(ndim):
         dims.append(shape[i])
     dtype = np.dtype(dtype_num_to_char(metadata.type))
     np_bytes_list = metadata.mdata
@@ -22,6 +22,7 @@ def metadata_to_nparray(metadata):
     np_array = np.frombuffer(np_bytes_array, dtype)
     np_array = np_array.reshape(tuple(dims))
     return np_array
+
 
 def nparray_to_metadata(np_array):
     metadata = DDSMetaData()
@@ -31,11 +32,11 @@ def nparray_to_metadata(np_array):
     metadata.total_size = np_array.size
     shape_list = list(np_array.shape)
     for i in range(5 - len(shape_list)):
-      shape_list.append(0)
+        shape_list.append(0)
     metadata.dims = shape_list
     strides_list = list(np_array.strides)
     for i in range(5 - len(strides_list)):
-      strides_list.append(0)
+        trides_list.append(0)
     metadata.strides = strides_list
     np_bytes = np_array.tobytes()
     np_bytes_array = np.frombuffer(np_bytes, np.byte)
