d7b03809e2bafb2ef696dd619a63d41666081c03
Miao, Hongda <hongda.miao@intel.com>
2022-12-05 10:18:30 +0800

    Fix msglib poetry install
    Signed-off-by: Miao, Hongda <hongda.miao@intel.com>

--- src/lava/magma/runtime/message_infrastructure/setup.py
+++ src/lava/magma/runtime/message_infrastructure/setup.py
@@ -132,9 +132,9 @@ class CMakeBuild(build_ext):
 # logic and declaration,
 # and simpler if you include description/version in a file.
 setup(
-    name="message_infrastructure",
+    name="MessageInfrastructurePywrapper",
     version="0.2.1",
-    ext_modules=[CMakeExtension("message_infrastructure.message_infrastructure")],
+    ext_modules=[CMakeExtension("message_infrastructure.MessageInfrastructurePywrapper")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
     extras_require={"test": ["pytest>=5.2"]},
