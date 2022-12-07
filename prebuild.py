f0d36fd36ca014409847b9948cdaf902e219109b
Miao, Hongda <hongda.miao@intel.com>
2022-12-07 18:47:12 +0800

    fix pylint error

--- prebuild.py
+++ prebuild.py
@@ -4,6 +4,7 @@ import numpy
 import subprocess  # nosec
 import sys
 
+
 class CMake:
     def __init__(self, sourcedir, targetdir):
         self.sourcedir = os.path.abspath(sourcedir)
@@ -17,7 +18,7 @@ class CMake:
         self.temp_path = os.path.join(os.path.abspath(""), "build")
         if not os.path.exists(self.temp_path):
             os.makedirs(self.temp_path)
-            
+
     def _set_cmake_args(self):
         debug = int(os.environ.get("DEBUG", 0))
         cfg = "Debug" if debug else "Release"
@@ -27,8 +28,8 @@ class CMake:
             f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
         ]
         if "CMAKE_ARGS" in os.environ:
-            self.cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ")
-                           if item]
+            self.cmake_args += [item for item in
+                                os.environ["CMAKE_ARGS"].split(" ") if item]
         # Set numpy include header to cpplib
         self.cmake_args += [
             f"-DNUMPY_INCLUDE_DIRS={numpy.get_include()}"]
@@ -45,9 +46,10 @@ class CMake:
         subprocess.check_call([self.cmake_command, self.sourcedir] + self.cmake_args, cwd=self.temp_path, env=self.env)  # nosec # noqa
         subprocess.check_call([self.cmake_command, "--build", "."] + self.build_args,  cwd=self.temp_path, env=self.env)  # nosec # noqa
 
+
 if __name__ == '__main__':
     base_runtime_path = "src/lava/magma/runtime/"
-    sourcedir=f"{base_runtime_path}_c_message_infrastructure"
-    targetdir=f"{base_runtime_path}message_infrastructure"
+    sourcedir = f"{base_runtime_path}_c_message_infrastructure"
+    targetdir = f"{base_runtime_path}message_infrastructure"
     cmake = CMake(sourcedir, targetdir)
     cmake.run()
