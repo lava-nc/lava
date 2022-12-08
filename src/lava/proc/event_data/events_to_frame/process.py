099259f1c413fc1b13d2fff124640c37b9c85d68
Mathis Richter <mathis.richter@intel.com>
2022-12-08 16:21:18 +0100

    Reviewed EventsToFrame Process and unit tests.
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>

--- src/lava/proc/event_data/events_to_frame/process.py
+++ src/lava/proc/event_data/events_to_frame/process.py
@@ -13,10 +13,18 @@ class EventsToFrame(AbstractProcess):
     frame.
 
     Output shape can be either (W, H, 1) or (W, H, 2).
-        (1) If output shape is (W, H, 1), input is assumed to be unary.
-        (2) If output shape is (W, H, 2), input is assumed to be binary.
-        Negative events are represented by 1s in first channel.
-        Positive events are represented by 1s in second channel.
+
+    (1) If output shape is (W, H, 1), the event-based input data is assumed
+    to use unary polarity (all events are encoded with polarity 1). In this
+    case, the output frame has 1 at all coordinates that registered an event
+    and zero everywhere else.
+
+    (2) If output shape is (W, H, 2), the event-based input data is assumed
+    to use binary polarity (negative events have polarity values of 0, positive
+    events have polarity values of 1). In this case, the output frame encodes
+    negative events by values of 1 in the first channel and positive events
+    by values of 1 in the second channel. All other coordinates are set to
+    zero.
 
     Parameters
     ----------
@@ -42,7 +50,7 @@ class EventsToFrame(AbstractProcess):
 
     @staticmethod
     def _validate_shape_in(shape_in: ty.Tuple[int]) -> None:
-        """Validate that a given shape is of the form (max_num_events, ) where
+        """Validate that a given shape is of the form (max_num_events,), where
         max_num_events is strictly positive.
 
         Parameters
@@ -52,14 +60,13 @@ class EventsToFrame(AbstractProcess):
         """
         if len(shape_in) != 1:
             raise ValueError(f"Expected shape_in to be of the form "
-                             f"(max_num_events, ). "
-                             f"Found {shape_in=}.")
+                             f"(max_num_events,); "
+                             f"got {shape_in=}.")
 
         if shape_in[0] <= 0:
             raise ValueError(f"Expected max number of events "
-                             f"(first element of shape_in) to be strictly "
-                             f"positive. "
-                             f"Found {shape_in=}.")
+                             f"(first element of shape_in) to be positive; "
+                             f"got {shape_in=}.")
 
     @staticmethod
     def _validate_shape_out(shape_out: ty.Tuple[int, int, int]) -> None:
@@ -72,17 +79,16 @@ class EventsToFrame(AbstractProcess):
             Shape to validate.
         """
         if not len(shape_out) == 3:
-            raise ValueError(f"Expected shape_out to be 3D. "
-                             f"Found {shape_out=}.")
+            raise ValueError(f"Expected shape_out to be 3D; "
+                             f"got {shape_out=}.")
 
         if not (shape_out[2] == 1 or shape_out[2] == 2):
             raise ValueError(f"Expected number of channels "
                              f"(third element of shape_out) to be either "
-                             f"1 or 2. "
-                             f"Found {shape_out=}.")
+                             f"1 or 2; "
+                             f"got {shape_out=}.")
 
         if shape_out[0] <= 0 or shape_out[1] <= 0:
             raise ValueError(f"Expected width and height "
                              f"(first and second elements of shape_out) to be "
-                             f"strictly positive. "
-                             f"Found {shape_out=}.")
+                             f"positive; got {shape_out=}.")
--- tests/lava/proc/event_data/events_to_frame/test_process.py
+++ tests/lava/proc/event_data/events_to_frame/test_process.py
@@ -8,52 +8,60 @@ from lava.proc.event_data.events_to_frame.process import EventsToFrame
 
 
 class TestProcessEventsToFrame(unittest.TestCase):
-    def test_init(self):
+    def test_init(self) -> None:
         """Tests instantiation of SparseToDense for a 3D output."""
         to_frame = EventsToFrame(shape_in=(43200,),
                                  shape_out=(240, 180, 1))
 
         self.assertIsInstance(to_frame, EventsToFrame)
 
-    def test_invalid_shape_in_throws_exception(self):
-        """Tests whether a shape_in argument that isn't (n,) throws
-        an exception."""
+    def test_invalid_shape_in_throws_exception(self) -> None:
+        """Tests whether an exception is thrown if shape_in is not of the
+        form (n,)."""
+        invalid_shape_in = (43200, 1)
         with(self.assertRaises(ValueError)):
-            EventsToFrame(shape_in=(43200, 1),
+            EventsToFrame(shape_in=invalid_shape_in,
                           shape_out=(240, 180, 1))
 
-    def test_invalid_shape_out_throws_exception(self):
-        """Tests whether an exception is thrown when a 1d or 4d value
-        for the shape_out argument is given."""
-        with(self.assertRaises(ValueError)):
-            EventsToFrame(shape_in=(43200,),
-                          shape_out=(240,))
-
-        with(self.assertRaises(ValueError)):
-            EventsToFrame(shape_in=(43200,),
-                          shape_out=(240, 180, 3))
-
-        with(self.assertRaises(ValueError)):
-            EventsToFrame(shape_in=(43200,),
-                          shape_out=(240, 180, 2, 1))
-
-    def test_negative_size_shape_in_throws_exception(self):
+    def test_negative_size_shape_in_throws_exception(self) -> None:
         """Tests whether an exception is thrown when a negative integer for
-        the shape_in argument is given"""
+        the shape_in argument is given."""
+        invalid_shape_in = (-43200,)
         with(self.assertRaises(ValueError)):
-            EventsToFrame(shape_in=(-43200,),
+            EventsToFrame(shape_in=invalid_shape_in,
                           shape_out=(240, 180))
 
-    def test_negative_width_or_height_shape_out_throws_exception(self):
-        """Tests whether an exception is thrown when a negative width or height
-        for the shape_out argument is given"""
+    def test_invalid_shape_out_dimensionality_throws_exception(self) -> None:
+        """Tests whether an exception is thrown when the dimensionality of the
+        shape_out parameter is not 3."""
+        invalid_shape_out = (240, 180)
         with(self.assertRaises(ValueError)):
             EventsToFrame(shape_in=(43200,),
-                          shape_out=(-240, 180))
+                          shape_out=invalid_shape_out)
 
+    def test_invalid_number_of_channels_throws_exception(self) -> None:
+        """Tests whether an exception is thrown when the number of channels is
+        larger than 2."""
+        invalid_channel_size = 3
         with(self.assertRaises(ValueError)):
             EventsToFrame(shape_in=(43200,),
-                          shape_out=(240, -180))
+                          shape_out=(240, 180, invalid_channel_size))
+
+    def test_negative_width_in_shape_out_throws_exception(self) -> None:
+        """Tests whether an exception is thrown when a negative width is
+        specified for the shape_out parameter."""
+        invalid_width = -240
+        with(self.assertRaises(ValueError)):
+            EventsToFrame(shape_in=(43200,),
+                          shape_out=(invalid_width, 180))
+
+    def test_negative_height_in_shape_out_throws_exception(self) -> None:
+        """Tests whether an exception is thrown when a negative height is
+        specified for the shape_out parameter."""
+        invalid_height = -180
+        with(self.assertRaises(ValueError)):
+            EventsToFrame(shape_in=(43200,),
+                          shape_out=(240, invalid_height))
 
 
 if __name__ == '__main__':
