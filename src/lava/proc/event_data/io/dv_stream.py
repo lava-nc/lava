dc02db1bccb1c9754461e59d3294f6624bc2baad
Mathis Richter <mathis.richter@intel.com>
2022-12-08 10:23:26 +0100

    Cleaned up DvStream and tests.
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>

--- src/lava/proc/event_data/io/dv_stream.py
+++ src/lava/proc/event_data/io/dv_stream.py
@@ -1,4 +1,9 @@
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+# See: https://spdx.org/licenses/
+
 import typing as ty
+from dv import NetworkNumpyEventPacketInput
 
 from lava.magma.core.decorator import implements, requires
 from lava.magma.core.model.py.model import PyLoihiProcessModel
@@ -8,68 +13,56 @@ from lava.magma.core.process.ports.ports import OutPort
 from lava.magma.core.process.process import AbstractProcess
 from lava.magma.core.resources import CPU
 from lava.magma.core.sync.protocols.loihi_protocol import LoihiProtocol
-from dv import NetworkNumpyEventPacketInput
+
 
 class DvStream(AbstractProcess):
-    """
-    Parameters
-    ----------
-
-    """
     def __init__(self,
                  *,
                  address: str,
                  port: int,
-                 shape_out: ty.Tuple[int]) -> None:
+                 shape_out: ty.Tuple[int],
+                 **kwargs) -> None:
         super().__init__(address=address,
                          port=port,
-                         shape_out=shape_out)
-        self._validate_shape_out(shape_out)
-        self._validate_port(port)
+                         shape_out=shape_out,
+                         **kwargs)
         self._validate_address(address)
+        self._validate_port(port)
+        self._validate_shape(shape_out)
+
         self.out_port = OutPort(shape=shape_out)
-
-
-    @staticmethod
-    def _validate_shape_out(shape_out: ty.Tuple[int]) -> None:
-        """
-        Checks whether the given shape is valid and that the size given
-        is not a negative number. Raises relevant exception if not
-        """
-        if len(shape_out) != 1:
-            raise ValueError(f"Shape of the OutPort should be (n,). "
-                             f"{shape_out} was given.")
-        if shape_out[0] <= 0:
-            raise ValueError(f"Max number of events should be positive. "
-                             f"{shape_out} was given.")
-
-    @staticmethod
-    def _validate_port(port: int) -> None:
-        """
-        Check whether the given port is valid. Raises relevant exception if not
-        """
-
-        if not (0 <= port <= 65535):
-            raise ValueError(f"Port should be between 0 and 65535"
-                             f"{port} was given.")
 
     @staticmethod
     def _validate_address(address: str) -> None:
-        """
-        Check that address is not an ampty string. Raises relevant exception if not
-        """
-
+        """Check that address is not an empty string or None."""
         if not address:
-            raise ValueError("Address should not be empty")
+            raise ValueError("Address parameter not specified."
+                             "The address must be an IP address or domain.")
+
+    @staticmethod
+    def _validate_port(port: int) -> None:
+        """Check whether the given port number is valid."""
+        _min = 0
+        _max = 65535
+        if not (_min <= port <= _max):
+            raise ValueError(f"Port number must be an integer between {_min=} "
+                             f"and {_max=}; got {port=}.")
+
+    @staticmethod
+    def _validate_shape(shape: ty.Tuple[int]) -> None:
+        """Check that shape one-dimensional with a positive size."""
+        if len(shape) != 1:
+            raise ValueError(f"Shape of the OutPort should be (n,); "
+                             f"got {shape=}.")
+        if shape[0] <= 0:
+            raise ValueError(f"Size of the shape (maximum number of events) "
+                             f"must be positive; got {shape=}.")
 
 
 @implements(proc=DvStream, protocol=LoihiProtocol)
 @requires(CPU)
 class DvStreamPM(PyLoihiProcessModel):
-    """
-    Implementation of the DvStream process on Loihi, with sparse
-    representation of events.
-    """
+    """Python ProcessModel of the DvStream Process"""
     out_port: PyOutPort = LavaPyType(PyOutPort.VEC_SPARSE, int)
 
     def __init__(self, proc_params: dict) -> None:
@@ -77,8 +70,9 @@ class DvStreamPM(PyLoihiProcessModel):
         self._address = proc_params["address"]
         self._port = proc_params["port"]
         self._shape_out = proc_params["shape_out"]
-        self._event_stream = proc_params.get("_event_stream")
+        self._event_stream = proc_params.get("event_stream")
         if not self._event_stream:
-            self._event_stream = NetworkNumpyEventPacketInput(address=self._address, port=self._port)
-
-
+            self._event_stream = NetworkNumpyEventPacketInput(
+                address=self._address,
+                port=self._port
+            )
--- tests/lava/proc/event_data/io/test_dv_stream.py
+++ tests/lava/proc/event_data/io/test_dv_stream.py
@@ -2,18 +2,16 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # See: https://spdx.org/licenses/
 
-from dv import AedatFile
-from dv.AedatFile import _AedatFileEventNumpyPacketIterator
+import unittest
 import numpy as np
 import typing as ty
-import unittest
 
 from lava.magma.core.process.process import AbstractProcess
-from lava.magma.core.process.ports.ports import InPort, OutPort
+from lava.magma.core.process.ports.ports import InPort
 from lava.magma.core.process.variable import Var
 from lava.magma.core.resources import CPU
 from lava.magma.core.sync.protocols.loihi_protocol import LoihiProtocol
-from lava.magma.core.model.py.ports import PyInPort, PyOutPort
+from lava.magma.core.model.py.ports import PyInPort
 from lava.magma.core.model.py.type import LavaPyType
 from lava.magma.core.decorator import implements, requires
 from lava.magma.core.model.py.model import PyLoihiProcessModel
@@ -23,101 +21,136 @@ from lava.proc.event_data.io.dv_stream import DvStream, DvStreamPM
 
 
 class TestProcessDvStream(unittest.TestCase):
-    def test_init(self):
-        """
-        Tests instantiation of AedatDataLoader.
-        """
-
+    def test_init(self) -> None:
+        """Tests instantiation of AedatDataLoader."""
         stream = DvStream(address="127.0.0.1",
                           port=7777,
-                          shape_out=(43200,))
+                          shape_out=(43200,),
+                          additional_kwarg=5)
 
         self.assertIsInstance(stream, DvStream)
-        self.assertEqual((43200,), stream.out_port.shape)
+        self.assertEqual(stream.out_port.shape, (43200,))
+        self.assertEqual(stream.proc_params["additional_kwarg"], 5)
 
-    def test_invalid_shape_throws_exception(self):
-        """
-        Tests whether a shape_out argument with an invalid shape throws an exception.
-        """
+    def test_invalid_shape_throws_exception(self) -> None:
+        """Tests whether a shape that is invalid (not one-dimensional) throws
+        an exception."""
+        invalid_shape = (240, 180)
         with(self.assertRaises(ValueError)):
             DvStream(address="127.0.0.1",
                      port=7777,
-                     shape_out=(240, 180))
+                     shape_out=invalid_shape)
 
-    def test_negative_size_throws_exception(self):
-        """
-        Tests whether a shape_out argument with a negative size throws an exception.
-        """
+    def test_negative_size_throws_exception(self) -> None:
+        """Tests whether a shape with a negative size throws an exception."""
+        invalid_shape = (-43200,)
         with(self.assertRaises(ValueError)):
             DvStream(address="127.0.0.1",
                      port=7777,
-                     shape_out=(-240,))
+                     shape_out=invalid_shape)
 
-    def test_negative_port_throws_exception(self):
-        """
-        Tests whether a port argument with a negative size throws an exception.
-        """
+    def test_negative_port_throws_exception(self) -> None:
+        """Tests whether a negative port throws an exception."""
+        min_port = 0
+        invalid_port = min_port - 1
         with(self.assertRaises(ValueError)):
             DvStream(address="127.0.0.1",
-                     port=-7777,
+                     port=invalid_port,
                      shape_out=(43200,))
 
-    def test_port_out_of_range_throws_exception(self):
-        """
-             Tests whether a port argument that is out of range throws an error.
-        """
+    def test_port_out_of_range_throws_exception(self) -> None:
+        """Tests whether a positive port that is too large throws an
+        exception."""
+        max_port = 65535
+        invalid_port = max_port + 1
         with(self.assertRaises(ValueError)):
             DvStream(address="127.0.0.1",
-                     port=7777777,
+                     port=invalid_port,
                      shape_out=(43200,))
 
-    def test_address_empty_string_throws_exception(self):
+    def test_address_empty_string_throws_exception(self) -> None:
+        """Tests whether an empty address throws an exception."""
+        invalid_address = ""
         with(self.assertRaises(ValueError)):
-            DvStream(address="",
+            DvStream(address=invalid_address,
                      port=7777,
                      shape_out=(43200,))
+
+
+class RecvSparse(AbstractProcess):
+    """Process that receives arbitrary sparse data.
+
+    Parameters
+    ----------
+    shape: tuple
+        Shape of the InPort and Vars.
+    """
+    def __init__(self,
+                 shape: ty.Tuple[int]) -> None:
+        super().__init__(shape=shape)
+
+        self.in_port = InPort(shape=shape)
+
+        self.data = Var(shape=shape, init=np.zeros(shape, dtype=int))
+        self.idx = Var(shape=shape, init=np.zeros(shape, dtype=int))
+
+
+@implements(proc=RecvSparse, protocol=LoihiProtocol)
+@requires(CPU)
+class PyRecvSparsePM(PyLoihiProcessModel):
+    """Receives sparse data from PyInPort and stores a padded version of
+    received data and indices in Vars."""
+    in_port: PyInPort = LavaPyType(PyInPort.VEC_SPARSE, int)
+
+    data: np.ndarray = LavaPyType(np.ndarray, int)
+    idx: np.ndarray = LavaPyType(np.ndarray, int)
+
+    def run_spk(self) -> None:
+        data, idx = self.in_port.recv()
+
+        self.data = np.pad(data,
+                           pad_width=(0, self.in_port.shape[0] - data.shape[0]))
+        self.idx = np.pad(idx,
+                          pad_width=(0, self.in_port.shape[0] - data.shape[0]))
+
+
+class MockPacketInput:
+    def __next__(self):
+        return {
+            "x": 35,
+            "y": 35,
+            "polarity": 0,
+        }
+
 
 class TestProcessModelDvStream(unittest.TestCase):
-    def test_init(self):
-        """
-        Tests instantiation of the DvStream process model.
-        """
-        proc_params = {
+    def setUp(self) -> None:
+        self.proc_params = {
             "address": "127.0.0.1",
             "port": 7777,
             "shape_out": (43200,),
+            "event_stream": MockPacketInput()
         }
 
-        pm = DvStreamPM(proc_params)
-
+    def test_init(self) -> None:
+        """Tests instantiation of the DvStream PyProcModel."""
+        pm = DvStreamPM(proc_params=self.proc_params)
         self.assertIsInstance(pm, DvStreamPM)
 
-    def test_run_spike(self):
-        class PacketInput(ty.Protocol):
-            def __next__(self):
-                ...
+    def test_run_spk(self) -> None:
+        max_num_events = 15
+        shape = (max_num_events,)
 
-        class MockPacketInput:
-            def __next__(self):
-                return {
-                    "x": 35,
-                    "y": 35,
-                    "polarity": 0,
-                }
+        dv_stream = DvStream(address="127.0.0.1",
+                             port=7777,
+                             shape_out=shape,
+                             event_stream=MockPacketInput())
+        recv_sparse = RecvSparse(shape=shape)
 
+        dv_stream.out_port.connect(recv_sparse.in_port)
 
-        proc_params = {
-            "address": "127.0.0.1",
-            "port": 7777,
-            "shape_out": (43200,),
-            "_event_stream": MockPacketInput()
-
-        }
-
-        pm = DvStreamPM(proc_params)
-        pm.run_spk()
-
-
+        dv_stream.run(condition=RunSteps(num_steps=1), run_cfg=Loihi1SimCfg())
+        dv_stream.stop()
 
 
 if __name__ == '__main__':
