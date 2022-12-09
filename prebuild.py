d6656ca957def5671a5e6c2ed2fe099e7fdf400a
Luo Xiaochuan <xiaochuan.luo@intel.com>
2022-12-09 15:42:35 +0800

    Fix cd fail at msglib (#530)
    Signed-off-by: luoxiaoc <xiaochuan.luo@intel.com>
    Co-authored-by: Miao, Hongda <hongda.miao@intel.com>

--- prebuild.py
+++ prebuild.py
@@ -9,10 +9,18 @@ class CMake:
     def __init__(self, sourcedir, targetdir):
         self.sourcedir = os.path.abspath(sourcedir)
         self.targetdir = os.path.abspath(targetdir)
-        self.cmake_command = "cmake"
+        self.env = os.environ.copy()
+        self.from_poetry = self._check_poetry()
+        self.cmake_command = ["poetry", "run", "cmake"] \
+            if self.from_poetry else ["cmake"]
         self.cmake_args = []
         self.build_args = []
-        self.env = os.environ.copy()
+
+    def _check_poetry(self):
+        exec_code = self.env.get('_', "").rsplit('/')[-1]
+        if exec_code == 'poetry':
+            return True
+        return False
 
     def _set_cmake_path(self):
         self.temp_path = os.path.join(os.path.abspath(""), "build")
@@ -22,9 +30,17 @@ class CMake:
     def _set_cmake_args(self):
         debug = int(os.environ.get("DEBUG", 0))
         cfg = "Debug" if debug else "Release"
+        if self.from_poetry:
+            python_env = subprocess.check_output(["poetry", "env", "info", "-p"]) \
+                .decode().strip() + "/bin/python3" # nosec # noqa
+            numpy_include_dir = subprocess.check_output(["poetry", "run", # nosec # noqa
+                "python3", "-c", "import numpy; print(numpy.get_include())"]).decode().strip() # nosec # noqa
+        else:
+            python_env = sys.executable
+            numpy_include_dir = numpy.get_include()
         self.cmake_args += [
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={self.targetdir}",
-            f"-DPYTHON_EXECUTABLE={sys.executable}",
+            f"-DPYTHON_EXECUTABLE={python_env}",
             f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
         ]
         if "CMAKE_ARGS" in os.environ:
@@ -32,7 +48,7 @@ class CMake:
                                 os.environ["CMAKE_ARGS"].split(" ") if item]
         # Set numpy include header to cpplib
         self.cmake_args += [
-            f"-DNUMPY_INCLUDE_DIRS={numpy.get_include()}"]
+            f"-DNUMPY_INCLUDE_DIRS={numpy_include_dir}"]
 
         # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
         # across all generators.
@@ -43,8 +59,8 @@ class CMake:
     def run(self):
         self._set_cmake_path()
         self._set_cmake_args()
-        subprocess.check_call([self.cmake_command, self.sourcedir] + self.cmake_args, cwd=self.temp_path, env=self.env)  # nosec # noqa
-        subprocess.check_call([self.cmake_command, "--build", "."] + self.build_args,  cwd=self.temp_path, env=self.env)  # nosec # noqa
+        subprocess.check_call([*self.cmake_command, self.sourcedir] + self.cmake_args, cwd=self.temp_path, env=self.env)  # nosec # noqa
+        subprocess.check_call([*self.cmake_command, "--build", "."] + self.build_args,  cwd=self.temp_path, env=self.env)  # nosec # noqa
 
 
 if __name__ == '__main__':
