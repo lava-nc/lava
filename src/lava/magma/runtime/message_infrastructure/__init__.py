ac7fe622d6d98779556ece98fea5b5f934ec9ca6
luoxiaoc <xiaochuan.luo@intel.com>
2023-02-08 13:18:13 +0800

    initialize pure py for msg passing
    Signed-off-by: luoxiaoc <xiaochuan.luo@intel.com>

--- src/lava/magma/runtime/message_infrastructure/__init__.py
+++ src/lava/magma/runtime/message_infrastructure/__init__.py
@@ -2,32 +2,68 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # See: https://spdx.org/licenses/
 
-from ctypes import CDLL, RTLD_GLOBAL
 import os
+import platform
 
 
-def load_library():
-    lib_name = 'libmessage_infrastructure.so'
-    here = os.path.abspath(__file__)
-    lib_path = os.path.join(os.path.dirname(here), lib_name)
-    if (os.path.exists(lib_path)):
-        CDLL(lib_path, mode=RTLD_GLOBAL)
-    else:
-        print("Warn: No library file")
-    extra_lib_folder = os.path.join(os.path.dirname(here), "install", "lib")
-    if (os.path.exists(extra_lib_folder)):
-        extra_libs = os.listdir(extra_lib_folder)
-        for lib in extra_libs:
-            if '.so' in lib and ('idl' not in lib):
-                lib_file = os.path.join(extra_lib_folder, lib)
-                CDLL(lib_file, mode=RTLD_GLOBAL)
+def _get_pure_py() -> bool:
+    pure_py_env = os.getenv("LAVA_PURE_PYTHON", 0)
+    system_name = platform.system().lower()
+    if system_name != "linux":
+        return True
+    return int(pure_py_env) > 0
 
 
-load_library()
+PURE_PYTHON_VERSION = _get_pure_py()
+
+if PURE_PYTHON_VERSION:
+    from abc import ABC, abstractmethod
 
 
-from lava.magma.runtime.message_infrastructure. \
-    MessageInfrastructurePywrapper import (  # noqa
+    class Channel(ABC):
+        @property
+        @abstractmethod
+        def src_port(self):
+            pass
+
+        @property
+        @abstractmethod
+        def dst_port(self):
+            pass
+    from .py_ports import AbstractTransferPort
+    from .pypychannel import (
+        SendPort,
+        RecvPort)
+
+    SupportGRPCChannel = False
+    SupportFastDDSChannel = False
+    SupportCycloneDDSChannel = False
+
+else:
+    from ctypes import CDLL, RTLD_GLOBAL
+
+
+    def load_library():
+        lib_name = 'libmessage_infrastructure.so'
+        here = os.path.abspath(__file__)
+        lib_path = os.path.join(os.path.dirname(here), lib_name)
+        if os.path.exists(lib_path):
+            CDLL(lib_path, mode=RTLD_GLOBAL)
+        else:
+            print("Warn: No library file")
+        extra_lib_folder = os.path.join(os.path.dirname(here), "install", "lib")
+        if os.path.exists(extra_lib_folder):
+            extra_libs = os.listdir(extra_lib_folder)
+            for lib in extra_libs:
+                if '.so' in lib and ('idl' not in lib):
+                    lib_file = os.path.join(extra_lib_folder, lib)
+                    CDLL(lib_file, mode=RTLD_GLOBAL)
+
+
+    load_library()
+
+    from lava.magma.runtime.message_infrastructure. \
+        MessageInfrastructurePywrapper import (  # noqa
         CppMultiProcessing,
         ProcessType,
         Actor,
@@ -39,26 +75,26 @@ from lava.magma.runtime.message_infrastructure. \
         support_fastdds_channel,
         support_cyclonedds_channel)
 
-from lava.magma.runtime.message_infrastructure.MessageInfrastructurePywrapper \
-    import ChannelType as ChannelBackend  # noqa: E402
+    from lava.magma.runtime.message_infrastructure.MessageInfrastructurePywrapper \
+        import ChannelType as ChannelBackend  # noqa: E402
 
-from .ports import (  # noqa: E402
-    SendPort,
-    Channel,
-    getTempSendPort,
-    getTempRecvPort)
+    from .ports import (  # noqa: E402
+        SendPort,
+        Channel,
+        getTempSendPort,
+        getTempRecvPort)
 
-ChannelQueueSize = 1
-SyncChannelBytes = 128
-SupportGRPCChannel = support_grpc_channel()
-SupportFastDDSChannel = support_fastdds_channel()
-SupportCycloneDDSChannel = support_cyclonedds_channel()
+    ChannelQueueSize = 1
+    SyncChannelBytes = 128
+    SupportGRPCChannel = support_grpc_channel()
+    SupportFastDDSChannel = support_fastdds_channel()
+    SupportCycloneDDSChannel = support_cyclonedds_channel()
 
-if SupportGRPCChannel:
-    from .ports import GetRPCChannel
-if SupportFastDDSChannel or SupportCycloneDDSChannel:
-    from .ports import GetDDSChannel
-    from lava.magma.runtime.message_infrastructure. \
-        MessageInfrastructurePywrapper import (
+    if SupportGRPCChannel:
+        from .ports import GetRPCChannel
+    if SupportFastDDSChannel or SupportCycloneDDSChannel:
+        from .ports import GetDDSChannel
+        from lava.magma.runtime.message_infrastructure. \
+            MessageInfrastructurePywrapper import (
             DDSTransportType,
             DDSBackendType)
---
+++ src/lava/magma/runtime/message_infrastructure/py_ports.py
@@ -0,0 +1,51 @@
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: LGPL 2.1 or later
+# See: https://spdx.org/licenses/
+import typing as ty
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+
+class AbstractTransferPort(ABC):
+    """Abstract base class for CSP channel."""
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def d_type(self) -> np.dtype:
+        pass
+
+    @property
+    @abstractmethod
+    def shape(self) -> ty.Tuple[int, ...]:
+        pass
+
+    @property
+    @abstractmethod
+    def size(self) -> int:
+        pass
+
+    @abstractmethod
+    def start(self):
+        pass
+
+    @abstractmethod
+    def join(self):
+        pass
+
+
+class AbstractSendPort(AbstractTransferPort):
+    @abstractmethod
+    def send(self, data: np.ndarray):
+        pass
+
+
+class AbstractRecvPort(AbstractTransferPort):
+    @abstractmethod
+    def recv(self) -> np.ndarray:
+        pass
