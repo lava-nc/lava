876da1adcb63e648bb71334b02907656d49e25b3
szc321 <108719242+szc321@users.noreply.github.com>
2023-02-13 16:25:27 +0800

    modify the dvs and realsense msg restoration in test_fastdds_from_ros_realsense.py (#614)
    Signed-off-by: szc321 <zongcai.shi@intel.com>
    Co-authored-by: Xu, He <he.xu@intel.com>

--- src/lava/magma/runtime/_c_message_infrastructure/examples/ros2/test_fastdds_from_ros_dvs.py
+++ src/lava/magma/runtime/_c_message_infrastructure/examples/ros2/test_fastdds_from_ros_dvs.py
@@ -23,34 +23,34 @@ from lava.magma.runtime.message_infrastructure import (
 )
 
 
-def numpy2pil(np_array: np.ndarray) -> Image:
-    img = Image.fromarray(np_array, 'RGB')
-    return img
-
-
-def realsense_msg_process(res):
+def dvs_msg_process(res):
     stamp = int.from_bytes(bytearray(np.flipud(res[0:8]).tolist()),
                            byteorder='big', signed=False)
-    channel = int.from_bytes(bytearray(np.flipud(res[8:12]).tolist()),
-                             byteorder='big', signed=False)
-    width = int.from_bytes(bytearray(np.flipud(res[12:16]).tolist()),
+    width = int.from_bytes(bytearray(np.flipud(res[8:12:]).tolist()),
                            byteorder='big', signed=False)
-    height = int.from_bytes(bytearray(np.flipud(res[16:20]).tolist()),
+    height = int.from_bytes(bytearray(np.flipud(res[12:16:]).tolist()),
                             byteorder='big', signed=False)
-    img_data = res[20:]
     print("stamp nsec = ", stamp)
-    print("channel = ", channel)
     print("width = ", width)
     print("height = ", height)
-    print("img_data = ", img_data)
-    # img = numpy2pil(img_data.reshape((height, width, channel)))
-    # img.show()
-    # img.close()
-    # time.sleep(0.01)
+    img_data = res[16:]
+    img_data = img_data.reshape(height, width)
+    print("img_data_shape = ", img_data.shape)
+    for i in range(img_data.shape[0]):
+        for j in range(img_data.shape[1]):
+            x = j
+            y = i
+            p = int(img_data[i][j])
+            if p == 1:
+                print("event_data [x = %d, y = %d, p = %d]"
+                      % (x, y, p))
+    dds_get_timestamp = time.time() * 1e9
+    print("dds_get_timestamp =", dds_get_timestamp)
+    print("dvs_ros_latency = %ld nsec", dds_get_timestamp - stamp)
 
 
 def test_ddschannel():
-    name = 'rt/camera/color/image_raw_dds'
+    name = 'rt/prophesee/PropheseeCamera_optical_frame/cd_events_buffer'
 
     dds_channel = GetDDSChannel(
         name,
@@ -62,7 +62,7 @@ def test_ddschannel():
     recv_port.start()
     for i in range(10):
         res = recv_port.recv()
-        realsense_msg_process(res)
+        dvs_msg_process(res)
     recv_port.join()
 
 
