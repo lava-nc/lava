91a13ef5fdb9b70a7b339cbd0c6fe8c38018cf1d
szc321 <zongcai.shi@intel.com>
2023-02-16 10:45:10 +0800

    amend msg_lib cpput
    Signed-off-by: szc321 <zongcai.shi@intel.com>

--- src/lava/magma/runtime/_c_message_infrastructure/test/test_channel.cc
+++ src/lava/magma/runtime/_c_message_infrastructure/test/test_channel.cc
@@ -25,32 +25,24 @@ MetaDataPtr ExpectData() {
 }
 
 void SendProc(AbstractSendPortPtr send_port,
-              MetaDataPtr data,
-              AbstractActor* actor_ptr) {
+              MetaDataPtr data) {
   AbstractActor::StopFn stop_fn;
-  actor_ptr->SetStopFn(stop_fn);
   std::cout << "Here I am" << std::endl;
 
   // Sends data
   send_port->Start();
   send_port->Send(data);
   send_port->Join();
-
-  actor_ptr->SetStatus(ActorStatus::StatusStopped);
   std::cout << "Status STOPPED" << std::endl;
 }
 
-void RecvProc(AbstractRecvPortPtr recv_port, AbstractActor* actor_ptr) {
-  AbstractActor::StopFn stop_fn;
-  actor_ptr->SetStopFn(stop_fn);
+void RecvProc(AbstractRecvPortPtr recv_port) {
   std::cout << "Here I am (RECV)" << std::endl;
 
   // Returns received data
   recv_port->Start();
   auto recv_data = recv_port->Recv();
   recv_port->Join();
-
-  actor_ptr->SetStatus(ActorStatus::StatusStopped);
 
   // if (recv_data != Data()) {
   //   std::cout << "Received Data is incorrect" << std::endl;
@@ -84,13 +76,11 @@ TEST(TestSharedMemory, SharedMemSendReceive) {
   auto data = ExpectData();
   auto send_bound_fn = std::bind(&SendProc,
                                  send_port,
-                                 data,
-                                 std::placeholders::_1);
+                                 data);
   send_target_fn = send_bound_fn;
 
   auto recv_bound_fn = std::bind(&RecvProc,
-                                 recv_port,
-                                 std::placeholders::_1);
+                                 recv_port);
   recv_target_fn = recv_bound_fn;
 
   mp.BuildActor(send_target_fn);
