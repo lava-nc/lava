7a19ca6b88b17b71a70370bf47801a39c837bbc8
luoxiaoc <xiaochuan.luo@intel.com>
2023-02-20 13:49:30 +0800

    test without abc
    Signed-off-by: luoxiaoc <xiaochuan.luo@intel.com>

--- src/lava/magma/core/model/model.py
+++ src/lava/magma/core/model/model.py
@@ -14,7 +14,7 @@ from lava.magma.core.resources import AbstractResource
 from lava.magma.core.sync.protocol import AbstractSyncProtocol
 
 
-class AbstractProcessModel(ABC):
+class AbstractProcessModel:
     """Represents a model that implements the behavior of a Process.
 
     ProcessModels enable seamless cross-platform execution of Processes. In
