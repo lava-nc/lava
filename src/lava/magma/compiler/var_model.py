6109ed8b27cbc4bc64709f2c11b580b0bc18680a
Mathis Richter <mathis.richter@intel.com>
2023-02-21 21:41:45 +0100

    Cleaned up license headers.
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>

--- src/lava/magma/compiler/var_model.py
+++ src/lava/magma/compiler/var_model.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2021-22 Intel Corporation
 # SPDX-License-Identifier: LGPL 2.1 or later
 # See: https://spdx.org/licenses/
+
 from __future__ import annotations
 
 import typing as ty
