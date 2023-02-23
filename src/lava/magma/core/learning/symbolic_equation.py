7a9f6ab59ee05f65620811a8752c549c5fed37d3
Xu, He <he.xu@intel.com>
2023-02-23 08:01:40 +0000

    Remove useless type conversion
    Signed-off-by: Xu, He <he.xu@intel.com>

--- src/lava/magma/core/learning/symbolic_equation.py
+++ src/lava/magma/core/learning/symbolic_equation.py
@@ -836,7 +836,7 @@ class SymbolicEquation:
         new_symbol = None
 
         for symbol in SYMBOL_CLASSES:
-            new_symbol, expression = symbol.find(str(expression))
+            new_symbol, expression = symbol.find(expression)
 
             if isinstance(new_symbol, Expression):
                 new_symbol.symbol_list = self._parse(new_symbol.sub_expr)
