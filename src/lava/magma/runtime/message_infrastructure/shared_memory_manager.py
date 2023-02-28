80f10b549a0123fab1c7813742b59baedb9d6faf
ssgier <sandro.sgier@gmail.com>
2023-02-28 19:32:03 +0100

    Improve naming

--- src/lava/magma/runtime/message_infrastructure/shared_memory_manager.py
+++ src/lava/magma/runtime/message_infrastructure/shared_memory_manager.py
@@ -2,9 +2,7 @@
 # SPDX-License-Identifier: LGPL 2.1 or later
 # See: https://spdx.org/licenses/
 
-from multiprocessing.managers import (SharedMemoryManager as
-                                      DelegateManager
-                                      )
+import multiprocessing.managers as managers
 from multiprocessing.shared_memory import SharedMemory
 import typing as ty
 
@@ -12,21 +10,22 @@ import typing as ty
 class SharedMemoryManager:
     def __init__(self) -> None:
         self._shared_memory_handles: ty.List[SharedMemory] = []
-        self._inner_shared_memory_manager: ty.Optional[DelegateManager] = (
-            DelegateManager())
+        self._manager: ty.Optional[
+            managers.SharedMemoryManager
+        ] = managers.SharedMemoryManager()
 
     def start(self) -> None:
-        self._inner_shared_memory_manager.start()
+        self._manager.start()
 
     def shutdown(self) -> None:
-        if self._inner_shared_memory_manager is not None:
-            for shm in self._shared_memory_handles:
-                shm.close()
+        if self._manager is not None:
+            for handle in self._shared_memory_handles:
+                handle.close()
             self._shared_memory_handles.clear()
-            self._inner_shared_memory_manager.shutdown()
-            self._inner_shared_memory_manager = None
+            self._manager.shutdown()
+            self._manager = None
 
     def create_shared_memory(self, size: int) -> SharedMemory:
-        shm = self._inner_shared_memory_manager.SharedMemory(size)
-        self._shared_memory_handles.append(shm)
-        return shm
+        handle = self._manager.SharedMemory(size)
+        self._shared_memory_handles.append(handle)
+        return handle
