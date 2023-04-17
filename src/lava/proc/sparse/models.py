320b75814c5fe4823a3c5db2c755f29e5645ec65
SveaMeyer13 <svea.meyer@tum.de>
2023-04-17 14:13:07 +0200

    delay sparse model + test

--- src/lava/proc/sparse/models.py
+++ src/lava/proc/sparse/models.py
@@ -3,7 +3,7 @@
 # See: https://spdx.org/licenses/
 
 import numpy as np
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, spmatrix
 
 from lava.magma.core.sync.protocols.loihi_protocol import LoihiProtocol
 from lava.magma.core.model.py.ports import PyInPort, PyOutPort
@@ -11,7 +11,7 @@ from lava.magma.core.model.py.type import LavaPyType
 from lava.magma.core.resources import CPU
 from lava.magma.core.decorator import implements, requires, tag
 from lava.magma.core.model.py.model import PyLoihiProcessModel
-from lava.proc.sparse.process import Sparse
+from lava.proc.sparse.process import Sparse, DelaySparse
 from lava.utils.weightutils import SignMode, determine_sign_mode,\
     truncate_weights, clip_weights
 
@@ -108,3 +108,112 @@ class AbstractPySparseModelBitAcc(PyLoihiProcessModel):
 @tag("bit_accurate_loihi", "fixed_pt")
 class PySparseModelBitAcc(AbstractPySparseModelBitAcc):
     pass
+
+
+
+
+class AbstractPyDelaySparseModel(PyLoihiProcessModel):
+    """Abstract Conn Process with Dense synaptic connections which incorporates
+    delays into the Conn Process.
+    """
+
+    @staticmethod
+    def get_del_wgts(weights, delays) -> spmatrix:
+        """
+        Use self.weights and self.delays to create a matrix where the
+        weights are separated by delay. Returns 2D matrix of form
+        (num_flat_output_neurons * max_delay + 1, num_flat_input_neurons) where
+        del_wgts[
+            k * num_flat_output_neurons : (k + 1) * num_flat_output_neurons, :
+        ]
+        contains the weights for all connections with a delay equal to k.
+        This allows for the updating of the activation buffer and updating
+        weights.
+        """
+        #return np.vstack([
+        #    np.where(delays == k, weights, 0)
+        #    for k in range(np.max(delays) + 1)
+        #])
+
+        weight_delay_row = []
+        weight_delay_column = []
+        weight_delay_data = []
+        for r in range(weights.shape[0]):
+            for ind in range(weights.indptr[r], weights.indptr[r + 1]):
+                col = weights.indices[ind]
+                weight_delay_row.append(r + (delays[r,col] * weights.shape[0]))
+                weight_delay_column.append(weights.indices[ind])
+                weight_delay_data.append(weights.data[ind])
+        return csr_matrix((weight_delay_data,(weight_delay_row, weight_delay_column)),
+                          shape=(weights.shape[0] * (delays.max()+1), weights.shape[1]))
+
+
+    def calc_act(self, s_in) -> np.ndarray:
+        """
+        Calculate the activations by performing del_wgts * s_in. This matrix
+        is then summed across each row to get the activations to the output
+        neurons for different delays. This activation vector is reshaped to a
+        matrix of the form
+        (n_flat_output_neurons * (max_delay + 1), n_flat_output_neurons)
+        which is then transposed to get the activation matrix.
+        """
+
+        multiplied = self.get_del_wgts(self.weights,
+                                       self.delays) * s_in
+        print(s_in)
+        print(multiplied)
+        summed = np.sum(multiplied, axis=1)
+        print(summed)
+        reshaped = np.reshape(summed, (np.max(self.delays) + 1, self.weights.shape[0])).T
+        print(reshaped)
+        return reshaped
+      #  return np.reshape(
+      #      np.sum(self.get_del_wgts(self.weights,
+      #                               self.delays) * s_in, axis=1),
+      #      (np.max(self.delays) + 1, self.weights.shape[0])).T
+
+    def update_act(self, s_in):
+        """
+        Updates the activations for the connection.
+        Clears first column of a_buff and rolls them to the last column.
+        Finally, calculates the activations for the current time step and adds
+        them to a_buff.
+        This order of operations ensures that delays of 0 correspond to
+        the next time step.
+        """
+        self.a_buff[:, 0] = 0
+        self.a_buff = np.roll(self.a_buff, -1)
+        self.a_buff += self.calc_act(s_in)
+
+
+@implements(proc=DelaySparse, protocol=LoihiProtocol)
+@requires(CPU)
+@tag("floating_pt")
+class PyDelayDenseModelFloat(AbstractPyDelaySparseModel):
+    """Implementation of Conn Process with Dense synaptic connections in
+    floating point precision. This short and simple ProcessModel can be used
+    for quick algorithmic prototyping, without engaging with the nuances of a
+    fixed point implementation. DelayDense incorporates delays into the Conn
+    Process.
+    """
+    s_in: PyInPort = LavaPyType(PyInPort.VEC_DENSE, bool, precision=1)
+    a_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, float)
+    a_buff: np.ndarray = LavaPyType(np.ndarray, float)
+    # weights is a 2D matrix of form (num_flat_output_neurons,
+    # num_flat_input_neurons) in C-order (row major).
+    weights: np.ndarray = LavaPyType(np.ndarray, float)
+    # delays is a 2D matrix of form (num_flat_output_neurons,
+    # num_flat_input_neurons) in C-order (row major).
+    delays: np.ndarray = LavaPyType(np.ndarray, int)
+    num_message_bits: np.ndarray = LavaPyType(np.ndarray, int, precision=5)
+
+    def run_spk(self):
+        # The a_out sent on a each timestep is a buffered value from dendritic
+        # accumulation at timestep t-1. This prevents deadlocking in
+        # networks with recurrent connectivity structures.
+        self.a_out.send(self.a_buff[:, 0])
+        if self.num_message_bits.item() > 0:
+            s_in = self.s_in.recv()
+        else:
+            s_in = self.s_in.recv().astype(bool)
+        self.update_act(s_in)
--- tests/lava/proc/sparse/test_model.py
+++ tests/lava/proc/sparse/test_model.py
@@ -2,16 +2,28 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # See: https://spdx.org/licenses/
 
-import unittest
-import numpy as np
 from scipy.sparse import csr_matrix
 
-from lava.proc.sparse.process import Sparse
 from lava.proc.dense.process import Dense
+from lava.proc.sparse.process import Sparse, DelaySparse
+from lava.proc.sparse.models import AbstractPyDelaySparseModel
 from lava.proc.io.source import RingBuffer as Source
 from lava.proc.io.sink import RingBuffer as Sink
 from lava.magma.core.run_configs import Loihi2SimCfg 
+import unittest
+import numpy as np
+from lava.magma.core.decorator import implements, requires, tag
+from lava.magma.core.model.py.model import PyLoihiProcessModel
+from lava.magma.core.model.py.ports import PyOutPort, PyInPort
+from lava.magma.core.model.py.type import LavaPyType
+from lava.magma.core.process.ports.ports import OutPort, InPort
+from lava.magma.core.process.process import AbstractProcess
+from lava.magma.core.process.variable import Var
+from lava.magma.core.resources import CPU
+from lava.magma.core.run_configs import RunConfig
 from lava.magma.core.run_conditions import RunSteps
+from lava.magma.core.sync.protocols.loihi_protocol import LoihiProtocol
+from lava.proc.dense.models import AbstractPyDelayDenseModel
 
 def create_network(input_data, conn, weights):
     source = Source(data=input_data)
@@ -387,3 +399,153 @@ class TestSparseProcessModelFixed(unittest.TestCase):
     #     self.assertIsInstance(weights_got_ts_2, csr_matrix)
     #     np.testing.assert_array_equal(weights_got_ts_1.toarray(), weights_init)
     #     np.testing.assert_array_equal(weights_got_ts_2.toarray(), weights_to_set_sparse.toarray())
+
+class VecSendandRecvProcess(AbstractProcess):
+    """
+    Process of a user-defined shape that sends an arbitrary vector
+
+    Process also listens for incoming connections via InPort a_in. This
+    allows the test Process to validate that network behavior won't deadlock
+    in the presence of recurrent connections.
+
+    Parameters
+    ----------
+    shape: tuple, shape of the process
+    vec_to_send: np.ndarray, vector of spike values to send
+    send_at_times: np.ndarray, vector bools. Send the `vec_to_send` at times
+    when there is a True
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        shape = kwargs.pop("shape", (1,))
+        vec_to_send = kwargs.pop("vec_to_send")
+        send_at_times = kwargs.pop("send_at_times")
+        num_steps = kwargs.pop("num_steps", 1)
+        self.shape = shape
+        self.num_steps = num_steps
+        self.vec_to_send = Var(shape=shape, init=vec_to_send)
+        self.send_at_times = Var(shape=(num_steps,), init=send_at_times)
+        self.s_out = OutPort(shape=shape)
+        self.a_in = InPort(shape=shape)  # enables recurrence test
+
+
+class VecRecvProcess(AbstractProcess):
+    """
+    Process that receives arbitrary vectors
+
+    Parameters
+    ----------
+    shape: tuple, shape of the process
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        shape = kwargs.get("shape", (1,))
+        self.shape = shape
+        self.s_in = InPort(shape=(shape[1],))
+        self.spk_data = Var(shape=shape, init=0)  # This Var expands with time
+
+
+@implements(proc=VecSendandRecvProcess, protocol=LoihiProtocol)
+@requires(CPU)
+# need the following tag to discover the ProcessModel using DenseRunConfig
+@tag('floating_pt')
+class PyVecSendModelFloat(PyLoihiProcessModel):
+    s_out: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, bool, precision=1)
+    a_in: PyInPort = LavaPyType(PyInPort.VEC_DENSE, float)
+    vec_to_send: np.ndarray = LavaPyType(np.ndarray, bool, precision=1)
+    send_at_times: np.ndarray = LavaPyType(np.ndarray, bool, precision=1)
+
+    def run_spk(self):
+        """
+        Send `spikes_to_send` if current time-step requires it
+        """
+        self.a_in.recv()
+
+        if self.send_at_times[self.time_step - 1]:
+            self.s_out.send(self.vec_to_send)
+        else:
+            self.s_out.send(np.zeros_like(self.vec_to_send))
+
+@implements(proc=VecRecvProcess, protocol=LoihiProtocol)
+@requires(CPU)
+# need the following tag to discover the ProcessModel using DenseRunConfig
+@tag('floating_pt')
+class PySpkRecvModelFloat(PyLoihiProcessModel):
+    s_in: PyInPort = LavaPyType(PyInPort.VEC_DENSE, bool, precision=1)
+    spk_data: np.ndarray = LavaPyType(np.ndarray, float)
+
+    def run_spk(self):
+        """Receive spikes and store in an internal variable"""
+        spk_in = self.s_in.recv()
+        self.spk_data[self.time_step - 1, :] = spk_in
+
+class TestDelayDenseProcessModel(unittest.TestCase):
+        """Tests for ProcessModels of Dense with synaptic delay."""
+
+        def test_matrix_weight_delay_expansion(self):
+            """"""
+            shape = (5, 4)
+            weights = np.zeros(shape, dtype=float)
+            weights[3, 3] = 1
+            weights[1,2] = 3
+            weights[3,1] = 1
+            delays = np.zeros(shape, dtype=int)
+            delays[3, 3] = 1
+            delays[1,2] = 3
+            delays[3,1] = 2
+
+            weights_sparse = csr_matrix(weights)
+            delays_sparse = csr_matrix(delays)
+            wgt_dly_dense = AbstractPyDelayDenseModel.get_del_wgts(weights, delays)
+            wgt_dly_sparse = AbstractPyDelaySparseModel.get_del_wgts(weights_sparse, delays_sparse)
+            self.assertTrue(np.all(wgt_dly_sparse==wgt_dly_dense))
+
+        def test_float_pm_buffer_delay(self):
+            """Tests floating point Dense ProcessModel connectivity and temporal
+            dynamics. All input 'neurons' from the VecSendandRcv fire
+            once at time t=4, and only 1 connection weight
+            in the Dense Process is non-zero. The value of the delay matrix for
+            this weight is 2. The non-zero connection should have an activation of
+            1 at timestep t=7.
+            """
+            shape = (3, 4)
+            num_steps = 8
+            # Set up external input to emulate every neuron spiking once on
+            # timestep 4
+            vec_to_send = np.ones((shape[1],), dtype=float)
+            send_at_times = np.repeat(False, (num_steps,))
+            send_at_times[3] = True
+            sps = VecSendandRecvProcess(shape=(shape[1],), num_steps=num_steps,
+                                        vec_to_send=vec_to_send,
+                                        send_at_times=send_at_times)
+            # Set up Dense Process with a single non-zero connection weight at
+            # entry [2, 2] of the connectivity matrix and a delay of 2 at entry
+            # [2, 2] in the delay matrix.
+            weights = np.zeros(shape, dtype=float)
+            weights[2, 2] = 1
+            delays = np.zeros(shape, dtype=int)
+            delays[2, 2] = 2
+            print(weights)
+            print(delays)
+            weights = csr_matrix(weights)
+            delays = csr_matrix(delays)
+            dense = DelaySparse(weights=weights, delays=delays)
+            # Receive neuron spikes
+            spr = VecRecvProcess(shape=(num_steps, shape[0]))
+            sps.s_out.connect(dense.s_in)
+            dense.a_out.connect(spr.s_in)
+            # Configure execution and run
+            rcnd = RunSteps(num_steps=num_steps)
+            rcfg = Loihi2SimCfg(select_tag='floating_pt')
+            dense.run(condition=rcnd, run_cfg=rcfg)
+            # Gather spike data and stop
+            spk_data_through_run = spr.spk_data.get()
+            dense.stop()
+            # Gold standard for the test
+            # a_out will be equal to 1 at timestep 7, because the dendritic
+            #  accumulators work on inputs from the previous timestep + 2.
+            expected_spk_data = np.zeros((num_steps, shape[0]))
+            expected_spk_data[6, 2] = 1.
+            self.assertTrue(np.all(expected_spk_data == spk_data_through_run))
