9d05f76bd0d49887c86e905e965d89512bedf4f9
szc321 <zongcai.shi@intel.com>
2023-05-08 17:32:52 +0800

    QueueSize=128
    Signed-off-by: szc321 <zongcai.shi@intel.com>

--- src/lava/magma/runtime/message_infrastructure/__init__.py
+++ src/lava/magma/runtime/message_infrastructure/__init__.py
@@ -88,7 +88,7 @@ else:
             support_fastdds_channel,
             support_cyclonedds_channel)
 
-    ChannelQueueSize = 1
+    ChannelQueueSize = 128
     SyncChannelBytes = 128
 
     from .ports import (  # noqa  # nosec
