a31bc932aa49b5e23397d8120db925cde2814ff5
Mathis Richter <mathis.richter@intel.com>
2023-05-13 02:58:16 +0200

    Full unit tests for lava_loihi module.
    Signed-off-by: Mathis Richter <mathis.richter@intel.com>

--- src/lava/utils/lava_loihi.py
+++ src/lava/utils/lava_loihi.py
@@ -2,18 +2,24 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # See: https://spdx.org/licenses/
 
+import typing as ty
 import importlib.util
 
 
-def is_installed() -> bool:
+def is_installed(module_name: ty.Optional[str] = None) -> bool:
     """Returns whether the Lava extension for Loihi is installed.
+
+    Parameters
+    ----------
+    module_name : Optional[str]
+        Name of the module to check for checking install.
 
     Returns
     -------
     bool
         True iff lava-loihi can be imported in this Python environment.
     """
-    lava_loihi_module = "lava.magma.compiler.subcompilers.nc"
+    lava_loihi_module = module_name or "lava.magma.compiler.subcompilers.nc"
     spec = importlib.util.find_spec(lava_loihi_module)
 
-    return True if spec is not None else False
+    return False if spec is None else True
