d8bae1fe61732e23d48056ff67de24641699600d
Elvin Hajizada <elvin.hajizada@intel.com>
2023-06-08 15:46:51 +0200

    removed duplicate code in prototypeLIF process; linting fixes

--- src/lava/proc/clp/prototype_lif/process.py
+++ src/lava/proc/clp/prototype_lif/process.py
@@ -13,64 +13,6 @@ from lava.magma.core.learning.learning_rule import Loihi2FLearningRule, \
 class PrototypeLIF(LearningLIF):
     """Prototype Leaky-Integrate-and-Fire (LIF) neural Process with learning
     enabled. Prototype neurons are central piece of the Continually Learning
-    Prototypes (CLP) algorithm.
+    Prototypes (CLP) algorithm. """
 
-        Parameters
-        ----------
-        shape : tuple(int)
-            Number and topology of LIF neurons.
-        u : float, list, numpy.ndarray, optional
-            Initial value of the neurons' current.
-        v : float, list, numpy.ndarray, optional
-            Initial value of the neurons' voltage (membrane potential).
-        du : float, optional
-            Inverse of decay time-constant for current decay. Currently, only a
-            single decay can be set for the entire population of neurons.
-        dv : float, optional
-            Inverse of decay time-constant for voltage decay. Currently, only a
-            single decay can be set for the entire population of neurons.
-        bias_mant : float, list, numpy.ndarray, optional
-            Mantissa part of neuron bias.
-        bias_exp : float, list, numpy.ndarray, optional
-            Exponent part of neuron bias, if needed. Mostly for fixed point
-            implementations. Ignored for floating point implementations.
-        vth : float, optional
-            Neuron threshold voltage, exceeding which, the neuron will spike.
-            Currently, only a single threshold can be set for the entire
-            population of neurons.
-        log_config: LogConfig, optional
-            Configure the amount of debugging output.
-        learning_rule: LearningRule
-            Defines the learning parameters and equation.
-        """
-
-    def __init__(
-            self,
-            *,
-            shape: ty.Tuple[int, ...],
-            u: ty.Optional[ty.Union[float, list, np.ndarray]] = 0,
-            v: ty.Optional[ty.Union[float, list, np.ndarray]] = 0,
-            du: ty.Optional[float] = 0,
-            dv: ty.Optional[float] = 0,
-            bias_mant: ty.Optional[ty.Union[float, list, np.ndarray]] = 0,
-            bias_exp: ty.Optional[ty.Union[float, list, np.ndarray]] = 0,
-            vth: ty.Optional[float] = 10,
-            name: ty.Optional[str] = None,
-            log_config: ty.Optional[LogConfig] = None,
-            learning_rule: ty.Optional[ty.Union[Loihi2FLearningRule,
-                                                Loihi3FLearningRule]] = None,
-            **kwargs,
-    ) -> None:
-        super().__init__(
-                shape=shape,
-                u=u,
-                v=v,
-                du=du,
-                dv=dv,
-                bias_mant=bias_mant,
-                bias_exp=bias_exp,
-                vth=vth,
-                name=name,
-                log_config=log_config,
-                learning_rule=learning_rule,
-                **kwargs)
+    pass
