66a16dee54b2557d3f65cd480c97ee807dc20478
Risbud, Sumedh <sumedh.risbud@intel.com>
2023-06-15 16:33:27 -0700

    Removed the utility to estimate Python object size
    Signed-off-by: Risbud, Sumedh <sumedh.risbud@intel.com>

--- src/lava/utils/profiler.py
+++ src/lava/utils/profiler.py
@@ -18,27 +18,6 @@ except ModuleNotFoundError:
                   "Currently no profiler is available.")
 
 
-def get_pyobj_size(obj, seen=None):
-    """Recursively finds size of objects"""
-    size = sys.getsizeof(obj)
-    if seen is None:
-        seen = set()
-    obj_id = id(obj)
-    if obj_id in seen:
-        return 0
-    # Important mark as seen *before* entering recursion to gracefully handle
-    # self-referential objects
-    seen.add(obj_id)
-    if isinstance(obj, dict):
-        size += sum([get_pyobj_size(v, seen) for v in obj.values()])
-        size += sum([get_pyobj_size(k, seen) for k in obj.keys()])
-    elif hasattr(obj, '__dict__'):
-        size += get_pyobj_size(obj.__dict__, seen)
-    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
-        size += sum([get_pyobj_size(i, seen) for i in obj])
-    return size
-
-
 class Profiler:
     """Base class for profiling execution time, energy and other
     metrics on different resources. Depending on the computing
