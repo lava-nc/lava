c1a8cdf538e19555bb638c1ae7afe4571d265272
Elvin Hajizada <elvin.hajizada@intel.com>
2023-06-20 16:33:24 +0200

    clp tutorial01 v1

---
+++ tutorials/in_depth/clp/utils.py
@@ -0,0 +1,106 @@
+# Copyright (C) 2021-22 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+# See: https://spdx.org/licenses/
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_spikes(spikes, figsize, legend, colors, title, num_steps):
+    offsets = list(range(1, len(spikes) + 1))
+    num_x_ticks = np.arange(0, num_steps+1, 25)
+    
+    plt.figure(figsize=figsize)
+
+    plt.eventplot(positions=spikes,
+                  lineoffsets=offsets,
+                  linelength=0.9,
+                  colors=colors)
+
+    plt.title(title)
+    plt.xlabel("Time steps")
+    plt.ylabel("Neurons")
+
+    plt.xticks(num_x_ticks)
+    plt.grid(which='minor', color='lightgrey', linestyle=':', linewidth=0.5)
+    plt.grid(which='major', color='lightgray', linewidth=0.8)
+    plt.minorticks_on()
+    
+    plt.yticks(ticks=offsets, labels=legend)
+
+    plt.show()
+
+
+def plot_time_series(time, time_series, ylabel, title, figsize, color):
+    plt.figure(figsize=figsize)
+    plt.step(time, time_series, color=color)
+   
+    plt.title(title)
+    plt.xlabel("Time steps")
+    plt.grid(which='minor', color='lightgrey', linestyle=':', linewidth=0.5)
+    plt.grid(which='major', color='lightgray', linewidth=0.8)
+    plt.minorticks_on()
+
+    plt.ylabel(ylabel)
+    
+    plt.show()
+
+
+def plot_multiple_time_series(time, time_series_list, ylabel,
+                              title, figsize, color, legend,
+                              leg_loc="upper left"):
+    plt.figure(figsize=figsize)
+    for i in range(time_series_list.shape[1]):
+        plt.step(time, time_series_list[:,i], label=legend[i], color=color[i])
+        
+    plt.title(title)
+    plt.xlabel("Time steps")
+    plt.ylabel(ylabel)
+    plt.grid(which='minor', color='lightgrey', linestyle=':', linewidth=0.5)
+    plt.grid(which='major', color='lightgray', linewidth=0.8)
+    plt.minorticks_on()
+    plt.xlim(0, time_series_list.shape[0])
+
+    plt.legend(loc=leg_loc)
+    
+    plt.show()
+
+
+def plot_spikes_time_series(time, time_series, spikes, figsize, legend,
+                            colors, title, num_steps):
+
+    offsets = list(range(1, len(spikes) + 1))
+    num_x_ticks = np.arange(0, num_steps+1, 25)
+    
+    plt.figure(figsize=figsize)
+    
+    plt.subplot(211)
+    plt.eventplot(positions=spikes, 
+                  lineoffsets=offsets,
+                  linelength=0.9,
+                  colors=colors)
+
+    plt.title("Spike Arrival")
+    plt.xlabel("Time steps")
+
+    plt.xticks(num_x_ticks)
+    plt.xlim(0, num_steps)
+    plt.grid(which='minor', color='lightgrey', linestyle=':', linewidth=0.5)
+    plt.grid(which='major', color='lightgray', linewidth=0.8)
+    plt.minorticks_on()
+    
+    plt.yticks(ticks=offsets, labels=legend)
+    plt.tight_layout(pad=3.0)
+
+    plt.subplot(212)
+    plt.step(time, time_series, color=color[-1])
+   
+    plt.title(title[0])
+    plt.xlabel("Time steps")
+    plt.grid(which='minor', color='lightgrey', linestyle=':', linewidth=0.5)
+    plt.grid(which='major', color='lightgray', linewidth=0.8)
+    plt.minorticks_on()
+    plt.margins(x=0)
+
+    plt.ylabel("Trace Value")
+    
+    plt.show()
