e3b17b1413d2a3234454d0676a99175c90344f5d
Elvin Hajizada <elvin.hajizada@intel.com>
2023-06-20 16:44:47 +0200

    a test for PrototypeLIF output spike after 3rd factor input

--- tests/lava/proc/clp/prototype_lif/test_models.py
+++ tests/lava/proc/clp/prototype_lif/test_models.py
@@ -110,3 +110,52 @@ class TestPrototypeLIFBitAccModel(unittest.TestCase):
         expected_result = np.zeros((n_protos, t_run))
         expected_result[0, 3:] = 127
         np.testing.assert_array_almost_equal(result, expected_result)
+
+    def test_neuron_outputs_spike_if_received_3rd_factor(self):
+        # Params
+        n_protos = 2
+        t_run = 20
+
+        # The array for 3rd factor input spike times
+        s_third_factor_in = np.zeros((n_protos, t_run))
+
+        # Inject a 3rd factor signal at the t=3
+        s_third_factor_in[0, 3] = 127
+
+        # Processes
+        # 3rd factor source input process (RingBuffer)
+        third_factor_input = Source(data=s_third_factor_in)
+
+        # Prototype Lif Process
+        prototypes = PrototypeLIF(du=4095,
+                                  dv=4095,
+                                  bias_mant=0,
+                                  bias_exp=0,
+                                  vth=1,
+                                  shape=(n_protos,),
+                                  name='lif_prototypes',
+                                  )
+        monitor = Monitor()
+
+        # Connections
+        third_factor_input.s_out.connect(prototypes.a_third_factor_in)
+
+        # Probe the y1 (post-synaptic trace)
+        monitor.probe(target=prototypes.s_out, num_steps=t_run)
+
+        # Run
+        run_cond = RunSteps(num_steps=t_run)
+        run_cfg = Loihi2SimCfg(select_tag="fixed_pt")
+
+        monitor.run(condition=run_cond, run_cfg=run_cfg)
+
+        # Get the probed data
+        result = monitor.get_data()
+        result = result[prototypes.name][prototypes.s_out.name].T
+
+        monitor.stop()
+        # Validate the post-synaptic trace: it gets updated to the value of
+        # the 3rd factor signal and then stays same
+        expected_result = np.zeros((n_protos, t_run))
+        expected_result[0, 3] = 1
+        np.testing.assert_array_equal(result, expected_result)
