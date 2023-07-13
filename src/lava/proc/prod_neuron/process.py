789d3893e98e70ebef42eabf678af4bc088e0299
Paxon <e.paxon.frady@intel.com>
2023-07-13 12:06:30 -0700

    fixed prod neuron license headers

--- src/lava/proc/prod_neuron/process.py
+++ src/lava/proc/prod_neuron/process.py
@@ -1,17 +1,5 @@
-# INTEL CORPORATION CONFIDENTIAL AND PROPRIETARY
-#
-# Copyright © 2021-2023 Intel Corporation.
-#
-# This software and the related documents are Intel copyrighted
-# materials, and your use of them is governed by the express
-# license under which they were provided to you (License). Unless
-# the License provides otherwise, you may not use, modify, copy,
-# publish, distribute, disclose or transmit  this software or the
-# related documents without Intel's prior written permission.
-#
-# This software and the related documents are provided as is, with
-# no express or implied warranties, other than those that are
-# expressly stated in the License.
+# Copyright (C) 2023 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
 # See: https://spdx.org/licenses/
 
 import numpy as np
