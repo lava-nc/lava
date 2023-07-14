76fafb87dee54799bcd95b4c9d690281a0884175
Paxon <e.paxon.frady@intel.com>
2023-07-14 08:59:48 -0700

    test for second channel of norm vec

--- tests/lava/proc/graded/test_graded.py
+++ tests/lava/proc/graded/test_graded.py
@@ -20,7 +20,6 @@ class TestGradedVecProc(unittest.TestCase):
     #@unittest.skipUnless(run_loihi2_tests, "Loihi2 unavailable.")
     #@unittest.skip("Skipping for merge.")
     def test_gradedvec_dot_dense(self):
-        print('graded dense')
         num_steps = 10
         v_thresh = 1
 
@@ -59,7 +58,6 @@ class TestGradedVecProc(unittest.TestCase):
         self.assertTrue(np.all(out_data[:, (3,7)] == expected_out[:,(2,6)]))
         
     def test_gradedvec_dot_sparse(self):
-        print('graded sparse')
         num_steps = 10
         v_thresh = 1
 
@@ -101,7 +99,6 @@ class TestGradedVecProc(unittest.TestCase):
         
 class TestInvSqrtProc(unittest.TestCase):
     def test_invsqrt_calc(self):
-        print('test_invsqrt')
         fp_base = 12# base of the decimal point
 
         num_steps = 25
@@ -150,7 +147,7 @@ class TestInvSqrtProc(unittest.TestCase):
         
 class TestNormVecDelayProc(unittest.TestCase):
     
-    def test_norm_vec_delay_out(self):
+    def test_norm_vec_delay_out1(self):
         fp_base = 12 # base of the decimal point
         weight_exp = 7
         num_steps = 10
@@ -213,7 +210,64 @@ class TestNormVecDelayProc(unittest.TestCase):
         
         # then there is one extra timestep from hardware
         self.assertTrue(np.all(expected_out[:,:-1] == out_data[:,1:]))
+ 
+    def test_norm_vec_delay_out2(self):
+        fp_base = 12# base of the decimal point
+        weight_exp = 7
+        num_steps = 10
+
+        weights1 = np.zeros((1,1)) 
+        weights1[0,0] = 1
+
+        weights1 *= 2**weight_exp
+        weights1 = weights1.astype('int')
+
+        weights2 = np.zeros((1,1)) 
+        weights2[0,0] = 0.5
+
+        weights2 *= 2**weight_exp
+        weights2 = weights2.astype('int')
         
+        inp_data1 = np.zeros((weights1.shape[1], num_steps))
+        inp_data2 = np.zeros((weights2.shape[1], num_steps))
+
+        inp_data1[:, 2] = 10
+        inp_data1[:, 6] = 30
+        inp_data2[:, :] = 20
+        
+        dense1 = Dense(weights=weights1, 
+               num_message_bits=24,
+               weight_exp=-weight_exp)
+        dense2 = Dense(weights=weights2, 
+                       num_message_bits=24,
+                       weight_exp=-weight_exp)
+
+        vec1 = NormVecDelay(shape=(weights1.shape[0],))
+
+        generator1 = io.source.RingBuffer(data=inp_data1)
+        generator2 = io.source.RingBuffer(data=inp_data2)
+        logger = io.sink.RingBuffer(shape=(weights1.shape[0],), 
+                                    buffer=num_steps)
+
+        
+        generator1.s_out.connect(dense1.s_in)
+        dense1.a_out.connect(vec1.a_in1)
+
+        generator2.s_out.connect(dense2.s_in)
+        dense2.a_out.connect(vec1.a_in2)
+
+        vec1.s2_out.connect(logger.a_in)
+        
+        vec1.run(condition=RunSteps(num_steps=num_steps), 
+                 run_cfg=Loihi2SimCfg(select_tag='fixed_pt'))
+        out_data = logger.data.get().astype('int')
+        vec1.stop()
+        
+        ch1 = (weights1 @ inp_data1) / 2**weight_exp
+        expected_out = ch1 ** 2
+        # then there is one extra timestep from hardware
+        self.assertTrue(np.all(expected_out[:,:-1] == out_data[:,1:]))
+
         
 if __name__ == '__main__':
     unittest.main()
