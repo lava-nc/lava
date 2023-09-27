42c3792b70d53b5b8baf9c26bd17940b3363c791
bamsumit <bam_sumit@hotmail.com>
2023-09-27 13:00:15 -0700

    Generalize int shape check in injector and extractor to take numpy ints (#792)
    * update refport unittest to always wait when it writes to port for consistent behavior
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Removed pyproject changes
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Fix to convolution tests. Fixed imcompatible mnist_pretrained for old python versions.
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Missing moudle parent fix
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Added ConvVarModel
    
    * Added iterable callback function
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Fix codacy issues in callback_fx.py
    
    * Fix linting in callback_fx.py
    
    * Fix codacy sig issue in callback_fx.py
    
    * Bugfix to pass the args by keyword
    
    * Delay Dense PyModel fix
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Fixed unittests
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * Fixed sparse delay
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * IO modules fixes
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    * IO modules fixes
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    
    ---------
    
    Signed-off-by: bamsumit <bam_sumit@hotmail.com>
    Co-authored-by: Joyesh Mishra <joyesh.mishra@intel.com>
    Co-authored-by: Marcus G K Williams <168222+mgkwill@users.noreply.github.com>

--- src/lava/proc/io/__init__.py
+++ src/lava/proc/io/__init__.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # See: https://spdx.org/licenses/
 
-from . import reset, source, sink, dataloader, encoder
+from . import reset, source, sink, dataloader, encoder, injector, extractor
 
-__all__ = ['reset', 'source', 'sink', 'dataloader', 'encoder']
+__all__ = ['reset', 'source', 'sink', 'dataloader', 'encoder', 'injector',
+           'extractor']
