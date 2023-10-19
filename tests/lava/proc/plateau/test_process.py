4be49183a3825101fd91fa7aa51bbda747b528df
kevin <kds300@psu.edu>
2023-10-19 12:44:02 -0400

    updated Plateau process unit test to specify integer inputs for dv_dend, dv_soma, vth_dend, and vth_soma
    Signed-off-by: kevin <kds300@psu.edu>

--- tests/lava/proc/plateau/test_process.py
+++ tests/lava/proc/plateau/test_process.py
@@ -14,17 +14,17 @@ class TestPlateauProcess(unittest.TestCase):
         """Tests instantiation of Plateau"""
         plat = Plateau(
             shape=(100,),
-            dv_dend=100.,
-            dv_soma=1.,
-            vth_dend=10.,
-            vth_soma=1.,
+            dv_dend=100,
+            dv_soma=1,
+            vth_dend=10,
+            vth_soma=1,
             up_dur=10,
             name="Plat"
         )
 
         self.assertEqual(plat.name, "Plat")
-        self.assertEqual(plat.dv_dend.init, 100.)
-        self.assertEqual(plat.dv_soma.init, 1.)
-        self.assertEqual(plat.vth_dend.init, 10.)
-        self.assertEqual(plat.vth_soma.init, 1.)
+        self.assertEqual(plat.dv_dend.init, 100)
+        self.assertEqual(plat.dv_soma.init, 1)
+        self.assertEqual(plat.vth_dend.init, 10)
+        self.assertEqual(plat.vth_soma.init, 1)
         self.assertEqual(plat.up_dur.init, 10)
