8e49cfafe2aabbb2f08db8565e1ac9f3b8fb5f42
Joyesh Mishra <joyesh.mishra@intel.com>
2024-02-20 15:38:03 -0800

    Enabled Caching in the Unit Test

--- src/lava/magma/compiler/channel_map.py
+++ src/lava/magma/compiler/channel_map.py
@@ -3,6 +3,8 @@
 # See: https://spdx.org/licenses/
 
 import itertools
+import os
+import pickle
 import typing as ty
 from collections import defaultdict
 from dataclasses import dataclass
@@ -11,6 +13,7 @@ from lava.magma.compiler.compiler_graphs import ProcGroup
 from lava.magma.compiler.utils import PortInitializer
 from lava.magma.core.process.ports.ports import AbstractPort
 from lava.magma.core.process.ports.ports import AbstractSrcPort, AbstractDstPort
+from lava.magma.core.process.process import AbstractProcess
 
 
 @dataclass(eq=True, frozen=True)
@@ -27,6 +30,10 @@ class Payload:
     dst_port_initializer: PortInitializer = None
 
 
+def lmt_init_id():
+    return -1
+
+
 class ChannelMap(dict):
     """The ChannelMap is used by the SubCompilers during compilation to
     communicate how they are planning to partition Processes onto their
@@ -35,7 +42,7 @@ class ChannelMap(dict):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._initializers_lookup = dict()
-        self._lmt_allocation_dict: ty.Dict[int, int] = defaultdict(lambda: -1)
+        self._lmt_allocation_dict: ty.Dict[int, int] = defaultdict(lmt_init_id)
 
     def __setitem__(
         self, key: PortPair, value: Payload, dict_setitem=dict.__setitem__
@@ -125,3 +132,77 @@ class ChannelMap(dict):
 
     def has_port_initializer(self, port) -> bool:
         return port in self._initializers_lookup
+
+    def write_to_cache(self,
+                       cache_object: ty.Dict[ty.Any, ty.Any],
+                       proc_to_procname_map: ty.Dict[AbstractProcess, str]):
+        cache_object["lmt_allocation"] = self._lmt_allocation_dict
+
+        initializers_serializable: ty.List[ty.Tuple[str, str,
+                                                    PortInitializer]] = []
+        port: AbstractPort
+        pi: PortInitializer
+        for port, pi in self._initializers_lookup.items():
+            procname = proc_to_procname_map[port.process]
+            if procname.startswith("Process_"):
+                msg = f"Unable to Cache. " \
+                      f"Please give unique names to every process. " \
+                      f"Violation Name: {procname=}"
+                raise Exception(msg)
+
+            initializers_serializable.append((procname, port.name, pi))
+        cache_object["initializers"] = initializers_serializable
+
+        cm_serializable: ty.List[ty.Tuple[ty.Tuple[str, str],
+                                          ty.Tuple[str, str],
+                                          Payload]] = []
+        port_pair: PortPair
+        payload: Payload
+        for port_pair, payload in self.items():
+            src_port: AbstractPort = ty.cast(AbstractPort, port_pair.src)
+            dst_port: AbstractPort = ty.cast(AbstractPort, port_pair.dst)
+            src_proc_name: str = proc_to_procname_map[src_port.process]
+            src_port_info = (src_proc_name, src_port.name)
+            dst_proc_name: str = proc_to_procname_map[dst_port.process]
+            dst_port_info = (dst_proc_name, dst_port.name)
+            if src_proc_name.startswith("Process_") or \
+                    dst_proc_name.startswith("Process_"):
+                msg = f"Unable to Cache. " \
+                      f"Please give unique names to every process. " \
+                      f"Violation Name: {src_proc_name=} {dst_proc_name=}"
+                raise Exception(msg)
+
+            cm_serializable.append((src_port_info, dst_port_info, payload))
+        cache_object["channelmap_dict"] = cm_serializable
+
+    def read_from_cache(self,
+                        cache_object: ty.Dict[ty.Any, ty.Any],
+                        procname_to_proc_map: ty.Dict[str, AbstractProcess]):
+        self._lmt_allocation_dict = cache_object["lmt_allocation"]
+        initializers_serializable = cache_object["initializers"]
+        cm_serializable = cache_object["channelmap_dict"]
+
+        for procname, port_name, pi in initializers_serializable:
+            process: AbstractProcess = procname_to_proc_map[procname]
+            port: AbstractPort = getattr(process, port_name)
+            self._initializers_lookup[port] = pi
+
+        src_port_info: ty.Tuple[str, str]
+        dst_port_info: ty.Tuple[str, str]
+        payload: Payload
+        for src_port_info, dst_port_info, payload in cm_serializable:
+            src_port_process: AbstractProcess = procname_to_proc_map[
+                src_port_info[0]]
+            src: AbstractPort = getattr(src_port_process,
+                                             src_port_info[1])
+            dst_port_process: AbstractProcess = procname_to_proc_map[
+                dst_port_info[0]]
+            dst: AbstractPort = getattr(dst_port_process,
+                                             dst_port_info[1])
+            for port_pair, pld in self.items():
+                s, d = port_pair.src, port_pair.dst
+                if s.name == src.name and d.name == dst.name and \
+                    s.process.name == src_port_process.name and \
+                    d.process.name == dst_port_process.name:
+                    pld.src_port_initializer = payload.src_port_initializer
+                    pld.dst_port_initializer = payload.dst_port_initializer
